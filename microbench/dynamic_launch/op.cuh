2db5ae57f6ea8f0b9a77ca393fda79a879b778f6
Raphael-Hao <raphaelhao@outlook.com>
2022-01-25 14:57:20 +0800

    update

--- microbench/dynamic_launch/op.cuh
+++ microbench/dynamic_launch/op.cuh
@@ -1,21 +1,35 @@
 #pragma once
-#include <cuda_runtime.h>
-
 #include "launch.cuh"
+
+#include <cuda_runtime.h>
 
 void __global__ set_flag(int *kFlag, int kValue) {
   int idx = threadIdx.x + blockIdx.x * blockDim.x;
-  if (idx == 1) {
+  if (idx == 0) {
     *kFlag = kValue;
   }
 }
 
-template <int kFlag>
-void __global__ kernel_add(float *a, float *b, float *c, int n,
+void __global__ simple_add(float *a, float *b, float *c, int n,
                            int *launch_flag) {
-  LAUCH_CHECK(launch_flag, kFlag);
-  int i = blockIdx.x * blockDim.x + threadIdx.x;
-  if (i < n) {
-    c[i] = a[i] + b[i];
+  int idx = blockIdx.x * blockDim.x + threadIdx.x;
+  printf("simple_add: %d\n", idx);
+  if (idx < n) {
+    c[idx] = a[idx] + b[idx];
   }
 }
+
+template <int kFlag>
+void __global__ dynamic_add(float *a, float *b, float *c, int n,
+                            int *launch_flag) {
+  LAUNCH_CHECK(launch_flag, kFlag);
+  int idx = blockIdx.x * blockDim.x + threadIdx.x;
+  if (idx == 0) {
+    printf("launch_flag = %d\n", kFlag);
+  }
+  if (idx < n) {
+    c[idx] = a[idx] + b[idx];
+  }
+}
+
+__global__ void helloFromGpu() { printf("Hello from GPU.\n"); }
