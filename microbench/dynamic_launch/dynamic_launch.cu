85c21dd1301367a303a5a3dc7b19210369c25837
Raphael-Hao <raphaelhao@outlook.com>
2022-01-25 18:20:56 +0800

    update

--- microbench/dynamic_launch/dynamic_launch.cu
+++ microbench/dynamic_launch/dynamic_launch.cu
@@ -1,4 +1,7 @@
+#include "../../include/common/argparse.h"
 #include "op.cuh"
+
+#include <assert.h>
 
 void SetConstantValue(float *h_data, int size, float value) {
   for (int i = 0; i < size; i++) {
@@ -17,39 +20,76 @@ void CheckResults(float *h_data, int size, float value, std::string name) {
 }
 
 int main(int argc, char **argv) {
-  CUDA_CHECK(cudaSetDevice(0));
-  const int test_iterations = 1;
-  helloFromGpu<<<1, 10>>>();
-  CUDA_CHECK(cudaDeviceSynchronize());
+  // parse arguments including sheduler flags, persistent L2cache
+  argparse::ArgumentParser program("dynamic kernel launch test");
+  program.add_argument("--device", "-d")
+      .help("device id to run on")
+      .scan<'i', int>()
+      .default_value(0);
+  program.add_argument("--iterations", "-i")
+      .help("iterations")
+      .scan<'i', int>()
+      .default_value(1000);
+  program.add_argument("--persist", "-p")
+      .help("persistent L2cache")
+      .default_value(false)
+      .implicit_value(true);
+  program.add_argument("--kernel_id", "-k")
+      .help("kernel id")
+      .scan<'i', int>()
+      .default_value(0);
+  try {
+    program.parse_args(argc, argv);
+  } catch (const std::runtime_error &err) {
+    std::cerr << err.what() << std::endl;
+    std::cerr << program;
+    std::exit(1);
+  }
 
+  // std::cout << program;
+  const int device_id = program.get<int>("--device");
+  const int test_iterations = program.get<int>("--iterations");
+  const bool persistent = program.get<bool>("--persist");
+  const int kernel_id = program.get<int>("--kernel_id");
+
+  // set device
+  printf("setting to device %d\n", device_id);
+  CUDA_CHECK(cudaSetDevice(device_id));
   // create stream
   cudaStream_t stream;
   CUDA_CHECK(cudaStreamCreate(&stream));
 
   int *d_schedule_flag;
   int *h_schedule_flag;
-  CUDA_CHECK(cudaMalloc(&d_schedule_flag, 16 * sizeof(int)));
-  CUDA_CHECK(cudaMallocHost(&h_schedule_flag, 16 * sizeof(int)));
+  auto size_schedule_flag = 16 * sizeof(int);
+  CUDA_CHECK(cudaMalloc(&d_schedule_flag, size_schedule_flag));
+  CUDA_CHECK(cudaMallocHost(&h_schedule_flag, size_schedule_flag));
 
   // set stream persistent L2 cache attributes
-  cudaStreamAttrID stream_attr_id = cudaStreamAttributeAccessPolicyWindow;
-  cudaDeviceProp prop;
-  CUDA_CHECK(cudaGetDeviceProperties(&prop, 0));
-  size_t persist_size =
-      min(int(prop.l2CacheSize * .1), prop.persistingL2CacheMaxSize);
-  if (persist_size > 0) {
-    printf("setting persistent L2 cache size to %zu bytes\n", persist_size);
-    cudaDeviceSetLimit(cudaLimitPersistingL2CacheSize, persist_size);
-    cudaStreamAttrValue stream_attr;
-    stream_attr.accessPolicyWindow.base_ptr = d_schedule_flag;
-    stream_attr.accessPolicyWindow.num_bytes = 16 * sizeof(int);
-    stream_attr.accessPolicyWindow.hitRatio = 1.0;
-    stream_attr.accessPolicyWindow.hitProp = cudaAccessPropertyPersisting;
-    stream_attr.accessPolicyWindow.missProp = cudaAccessPropertyStreaming;
-    CUDA_CHECK(cudaStreamSetAttribute(stream, stream_attr_id, &stream_attr));
-  } else {
-    printf("persistent L2 cache size is 0 bytes\n");
+  if (persistent) {
+    printf("trying to set persistent L2 cache\n");
+    cudaStreamAttrID stream_attr_id = cudaStreamAttributeAccessPolicyWindow;
+    cudaDeviceProp prop;
+    CUDA_CHECK(cudaGetDeviceProperties(&prop, 0));
+    size_t persist_size =
+        min(int(prop.l2CacheSize * .1), prop.persistingL2CacheMaxSize);
+    if (persist_size > 0) {
+      printf("setting persistent L2 cache size to %zu bytes\n",
+             size_schedule_flag * 2);
+      cudaDeviceSetLimit(cudaLimitPersistingL2CacheSize,
+                         size_schedule_flag * 2);
+      cudaStreamAttrValue stream_attr;
+      stream_attr.accessPolicyWindow.base_ptr = d_schedule_flag;
+      stream_attr.accessPolicyWindow.num_bytes = size_schedule_flag * 2;
+      stream_attr.accessPolicyWindow.hitRatio = 1.0;
+      stream_attr.accessPolicyWindow.hitProp = cudaAccessPropertyPersisting;
+      stream_attr.accessPolicyWindow.missProp = cudaAccessPropertyStreaming;
+      CUDA_CHECK(cudaStreamSetAttribute(stream, stream_attr_id, &stream_attr));
+    } else {
+      printf("failed to set persistent L2 cache due to its size is 0 bytes\n");
+    }
   }
+
   // create CUDA events
   cudaEvent_t start, stop;
   CUDA_CHECK(cudaEventCreate(&start));
@@ -83,12 +123,15 @@ int main(int argc, char **argv) {
   dim3 dim_grid(blocks_per_grid);
 
   // reset result data
+  printf("testing the lowwer bound");
   SetConstantValue(h_C, size_C, 0.0);
   // test lower bound
   CUDA_CHECK(cudaEventRecord(start, stream));
   for (int i = 0; i < test_iterations; i++) {
     // set scheduler flag
-    CUDA_CHECK(cudaMemsetAsync(d_schedule_flag, 0, 16 * sizeof(int), stream));
+    // CUDA_CHECK(cudaMemsetAsync(d_schedule_flag, 0, 16 * sizeof(int),
+    // stream));
+    set_flag<<<dim_grid, dim_block, 0, stream>>>(d_schedule_flag, kernel_id);
     // compute
     simple_add<<<dim_grid, dim_block, 0, stream>>>(d_A, d_B, d_C, size_A,
                                                    d_schedule_flag);
@@ -102,21 +145,38 @@ int main(int argc, char **argv) {
   CheckResults(h_C, size_C, 2.0, "lower bound");
   printf("lower bound: %f ms\n", elapsed_time / test_iterations);
 
+  printf("testing cpu dynamic scheduling");
   // reset result data
   SetConstantValue(h_C, size_C, 0.0);
   // test cpu dynamic scheduling
   CUDA_CHECK(cudaEventRecord(start, stream));
   for (int i = 0; i < test_iterations; i++) {
     // set scheduler flag
-    CUDA_CHECK(cudaMemsetAsync(d_schedule_flag, 0, 16 * sizeof(int), stream));
+    set_flag<<<dim_grid, dim_block, 0, stream>>>(d_schedule_flag, kernel_id);
     CUDA_CHECK(cudaMemcpyAsync(h_schedule_flag, d_schedule_flag, sizeof(int),
                                cudaMemcpyDeviceToHost, stream));
     CUDA_CHECK(cudaStreamSynchronize(stream));
     switch (*h_schedule_flag) {
-      case 0:
-      case 1:
-      case 2:
+      case 0: {
+        assert(kernel_id == 0);
+        simple_add<<<dim_grid, dim_block, 0, stream>>>(d_A, d_B, d_C, size_A,
+                                                       d_schedule_flag);
+        break;
+      }
+      case 1: {
+        assert(kernel_id == 1);
+        simple_add<<<dim_grid, dim_block, 0, stream>>>(d_A, d_B, d_C, size_A,
+                                                       d_schedule_flag);
+        break;
+      }
+      case 2: {
+        assert(kernel_id == 2);
+        simple_add<<<dim_grid, dim_block, 0, stream>>>(d_A, d_B, d_C, size_A,
+                                                       d_schedule_flag);
+        break;
+      }
       case 3: {
+        assert(kernel_id == 3);
         simple_add<<<dim_grid, dim_block, 0, stream>>>(d_A, d_B, d_C, size_A,
                                                        d_schedule_flag);
         break;
@@ -134,13 +194,14 @@ int main(int argc, char **argv) {
   CheckResults(h_C, size_C, 2.0, "cpu dynamic scheduling");
   printf("cpu dynamic scheduling: %f ms\n", elapsed_time / test_iterations);
 
+  printf("testing gpu dynamic scheduling");
   // reset result data
   SetConstantValue(h_C, size_C, 0.0);
   // test GPU dynamic scheduling
   CUDA_CHECK(cudaEventRecord(start, stream));
   for (int i = 0; i < test_iterations; i++) {
     // set scheduler flag
-    CUDA_CHECK(cudaMemsetAsync(d_schedule_flag, 0, 16 * sizeof(int), stream));
+    set_flag<<<dim_grid, dim_block, 0, stream>>>(d_schedule_flag, kernel_id);
     dynamic_add<0><<<dim_grid, dim_block, 0, stream>>>(d_A, d_B, d_C, size_A,
                                                        d_schedule_flag);
     dynamic_add<1><<<dim_grid, dim_block, 0, stream>>>(d_A, d_B, d_C, size_A,
@@ -148,7 +209,7 @@ int main(int argc, char **argv) {
     dynamic_add<2><<<dim_grid, dim_block, 0, stream>>>(d_A, d_B, d_C, size_A,
                                                        d_schedule_flag);
     dynamic_add<3><<<dim_grid, dim_block, 0, stream>>>(d_A, d_B, d_C, size_A,
-                                                       d_schedule_flag);
+     d_schedule_flag);
   }
   CUDA_CHECK(cudaEventRecord(stop, stream));
   CUDA_CHECK(cudaEventSynchronize(stop));
