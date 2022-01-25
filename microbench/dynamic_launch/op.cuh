91b76363c9d7dd3e5f5c9563317d4172238b2287
Raphael-Hao <raphaelhao@outlook.com>
2022-01-25 18:20:56 +0800

    update

--- microbench/dynamic_launch/op.cuh
+++ microbench/dynamic_launch/op.cuh
@@ -13,7 +13,6 @@ void __global__ set_flag(int *kFlag, int kValue) {
 void __global__ simple_add(float *a, float *b, float *c, int n,
                            int *launch_flag) {
   int idx = blockIdx.x * blockDim.x + threadIdx.x;
-  printf("simple_add: %d\n", idx);
   if (idx < n) {
     c[idx] = a[idx] + b[idx];
   }
@@ -24,12 +23,7 @@ void __global__ dynamic_add(float *a, float *b, float *c, int n,
                             int *launch_flag) {
   LAUNCH_CHECK(launch_flag, kFlag);
   int idx = blockIdx.x * blockDim.x + threadIdx.x;
-  if (idx == 0) {
-    printf("launch_flag = %d\n", kFlag);
-  }
   if (idx < n) {
     c[idx] = a[idx] + b[idx];
   }
 }
-
-__global__ void helloFromGpu() { printf("Hello from GPU.\n"); }
