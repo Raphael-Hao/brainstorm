54ea21f9cb4b736079f57f7cb3c9785a770fe8ef
Raphael-Hao <raphaelhao@outlook.com>
2022-02-09 23:14:19 +0800

    update

--- benchmark/thor/thor_onnx.py
+++ benchmark/thor/thor_onnx.py
@@ -14,38 +14,40 @@ from transformers.models.bert.configuration_bert import BertConfig
 
 def export_thor_model(model_filename):
     thor_config = ThorConfig()
+    thor_config.token_num = 64
+    thor_config.runtime = False
     thor_config.hidden_size = 512
     thor_config.intermediate_size = 1024
     thor_config.num_attention_heads = 8
     thor_config.num_hidden_layers = 1
     thor_config.expert_num = 2
-    thor_config.expert_type = "fusion"
+    thor_config.expert_type = "sparse_fusion"
     model_filename = (
         thor_config.expert_type + "_" + str(thor_config.expert_num) + "_" + model_filename
     )
     thor_encoder = ThorEncoder(thor_config)
     print(thor_encoder)
-    thor_encoder.cuda()
+    thor_encoder.cpu()
     thor_encoder.eval()
-    dummy_input = torch.rand(1, 64, 512, device="cuda", dtype=torch.float32)
+    dummy_input = torch.rand(1, 64, 512, device="cpu", dtype=torch.float32)
     dummy_output = thor_encoder(dummy_input)
     torch.onnx.export(
-        thor_encoder, dummy_input, model_filename, verbose=True, opset_version=11
+        thor_encoder, dummy_input, model_filename, opset_version=11
     )
     print(dummy_output[0].shape)
 
 
-def export_bert_model(model_filename):
-    bert_config = BertConfig()
-    bert_config.num_hidden_layers = 1
-    bert_encoder = BertEncoder(bert_config)
-    print(bert_encoder)
-    bert_encoder.cuda()
-    bert_encoder.eval()
-    dummy_input = torch.rand(1, 64, 768, device="cuda", dtype=torch.float32)
-    dummy_output = bert_encoder(dummy_input)
-    torch.onnx.export(bert_encoder, dummy_input, model_filename)
-    print(dummy_output[0].shape)
+# def export_bert_model(model_filename):
+#     bert_config = BertConfig()
+#     bert_config.num_hidden_layers = 1
+#     bert_encoder = BertEncoder(bert_config)
+#     print(bert_encoder)
+#     bert_encoder.cuda()
+#     bert_encoder.eval()
+#     dummy_input = torch.rand(1, 64, 768, device="cuda", dtype=torch.float32)
+#     dummy_output = bert_encoder(dummy_input)
+#     torch.onnx.export(bert_encoder, dummy_input, model_filename)
+#     print(dummy_output[0].shape)
 
 
 if __name__ == "__main__":
