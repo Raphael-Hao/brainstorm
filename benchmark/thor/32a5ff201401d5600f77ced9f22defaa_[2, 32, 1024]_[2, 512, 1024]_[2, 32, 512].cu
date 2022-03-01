b8b7cf21a6f3b4dbfc38df08aafecfbf657d4218
Raphael-Hao <raphaelhao@outlook.com>
2022-03-01 22:02:33 +0800

    update

---
+++ benchmark/thor/32a5ff201401d5600f77ced9f22defaa_[2, 32, 1024]_[2, 512, 1024]_[2, 32, 512].cu
@@ -0,0 +1,73 @@
+
+#ifdef _WIN32
+  using uint = unsigned int;
+  using uchar = unsigned char;
+  using ushort = unsigned short;
+  using int64_t = long long;
+  using uint64_t = unsigned long long;
+#else
+  #define uint unsigned int
+  #define uchar unsigned char
+  #define ushort unsigned short
+  #define int64_t long long
+  #define uint64_t unsigned long long
+#endif
+extern "C" __global__ void __launch_bounds__(32) default_function_kernel0(float* __restrict__ placeholder, float* __restrict__ placeholder1, float* __restrict__ T_batch_matmul_NT) {
+  float T_batch_matmul_NT_local[4];
+  __shared__ float placeholder_d_shared[1024];
+  __shared__ float placeholder_shared[2048];
+  T_batch_matmul_NT_local[(0)] = 0.000000e+00f;
+  T_batch_matmul_NT_local[(1)] = 0.000000e+00f;
+  T_batch_matmul_NT_local[(2)] = 0.000000e+00f;
+  T_batch_matmul_NT_local[(3)] = 0.000000e+00f;
+  for (int k_outer_outer = 0; k_outer_outer < 8; ++k_outer_outer) {
+    __syncthreads();
+    ((float4*)(placeholder_d_shared + ((((int)threadIdx.x) * 4))))[0] = ((float4*)(placeholder + (((((((int)blockIdx.x) >> 5) * 8192) + (k_outer_outer * 128)) + (((int)threadIdx.x) * 4)))))[0];
+    ((float4*)(placeholder_d_shared + (((((int)threadIdx.x) * 4) + 128))))[0] = ((float4*)(placeholder + ((((((((int)blockIdx.x) >> 5) * 8192) + (k_outer_outer * 128)) + (((int)threadIdx.x) * 4)) + 1024))))[0];
+    ((float4*)(placeholder_d_shared + (((((int)threadIdx.x) * 4) + 256))))[0] = ((float4*)(placeholder + ((((((((int)blockIdx.x) >> 5) * 8192) + (k_outer_outer * 128)) + (((int)threadIdx.x) * 4)) + 2048))))[0];
+    ((float4*)(placeholder_d_shared + (((((int)threadIdx.x) * 4) + 384))))[0] = ((float4*)(placeholder + ((((((((int)blockIdx.x) >> 5) * 8192) + (k_outer_outer * 128)) + (((int)threadIdx.x) * 4)) + 3072))))[0];
+    ((float4*)(placeholder_d_shared + (((((int)threadIdx.x) * 4) + 512))))[0] = ((float4*)(placeholder + ((((((((int)blockIdx.x) >> 5) * 8192) + (k_outer_outer * 128)) + (((int)threadIdx.x) * 4)) + 4096))))[0];
+    ((float4*)(placeholder_d_shared + (((((int)threadIdx.x) * 4) + 640))))[0] = ((float4*)(placeholder + ((((((((int)blockIdx.x) >> 5) * 8192) + (k_outer_outer * 128)) + (((int)threadIdx.x) * 4)) + 5120))))[0];
+    ((float4*)(placeholder_d_shared + (((((int)threadIdx.x) * 4) + 768))))[0] = ((float4*)(placeholder + ((((((((int)blockIdx.x) >> 5) * 8192) + (k_outer_outer * 128)) + (((int)threadIdx.x) * 4)) + 6144))))[0];
+    ((float4*)(placeholder_d_shared + (((((int)threadIdx.x) * 4) + 896))))[0] = ((float4*)(placeholder + ((((((((int)blockIdx.x) >> 5) * 8192) + (k_outer_outer * 128)) + (((int)threadIdx.x) * 4)) + 7168))))[0];
+    ((float4*)(placeholder_shared + ((((int)threadIdx.x) * 4))))[0] = ((float4*)(placeholder1 + ((((((((int)blockIdx.x) >> 7) * 524288) + ((((int)blockIdx.x) & 31) * 16384)) + (k_outer_outer * 128)) + (((int)threadIdx.x) * 4)))))[0];
+    ((float4*)(placeholder_shared + (((((int)threadIdx.x) * 4) + 128))))[0] = ((float4*)(placeholder1 + (((((((((int)blockIdx.x) >> 7) * 524288) + ((((int)blockIdx.x) & 31) * 16384)) + (k_outer_outer * 128)) + (((int)threadIdx.x) * 4)) + 1024))))[0];
+    ((float4*)(placeholder_shared + (((((int)threadIdx.x) * 4) + 256))))[0] = ((float4*)(placeholder1 + (((((((((int)blockIdx.x) >> 7) * 524288) + ((((int)blockIdx.x) & 31) * 16384)) + (k_outer_outer * 128)) + (((int)threadIdx.x) * 4)) + 2048))))[0];
+    ((float4*)(placeholder_shared + (((((int)threadIdx.x) * 4) + 384))))[0] = ((float4*)(placeholder1 + (((((((((int)blockIdx.x) >> 7) * 524288) + ((((int)blockIdx.x) & 31) * 16384)) + (k_outer_outer * 128)) + (((int)threadIdx.x) * 4)) + 3072))))[0];
+    ((float4*)(placeholder_shared + (((((int)threadIdx.x) * 4) + 512))))[0] = ((float4*)(placeholder1 + (((((((((int)blockIdx.x) >> 7) * 524288) + ((((int)blockIdx.x) & 31) * 16384)) + (k_outer_outer * 128)) + (((int)threadIdx.x) * 4)) + 4096))))[0];
+    ((float4*)(placeholder_shared + (((((int)threadIdx.x) * 4) + 640))))[0] = ((float4*)(placeholder1 + (((((((((int)blockIdx.x) >> 7) * 524288) + ((((int)blockIdx.x) & 31) * 16384)) + (k_outer_outer * 128)) + (((int)threadIdx.x) * 4)) + 5120))))[0];
+    ((float4*)(placeholder_shared + (((((int)threadIdx.x) * 4) + 768))))[0] = ((float4*)(placeholder1 + (((((((((int)blockIdx.x) >> 7) * 524288) + ((((int)blockIdx.x) & 31) * 16384)) + (k_outer_outer * 128)) + (((int)threadIdx.x) * 4)) + 6144))))[0];
+    ((float4*)(placeholder_shared + (((((int)threadIdx.x) * 4) + 896))))[0] = ((float4*)(placeholder1 + (((((((((int)blockIdx.x) >> 7) * 524288) + ((((int)blockIdx.x) & 31) * 16384)) + (k_outer_outer * 128)) + (((int)threadIdx.x) * 4)) + 7168))))[0];
+    ((float4*)(placeholder_shared + (((((int)threadIdx.x) * 4) + 1024))))[0] = ((float4*)(placeholder1 + (((((((((int)blockIdx.x) >> 7) * 524288) + ((((int)blockIdx.x) & 31) * 16384)) + (k_outer_outer * 128)) + (((int)threadIdx.x) * 4)) + 8192))))[0];
+    ((float4*)(placeholder_shared + (((((int)threadIdx.x) * 4) + 1152))))[0] = ((float4*)(placeholder1 + (((((((((int)blockIdx.x) >> 7) * 524288) + ((((int)blockIdx.x) & 31) * 16384)) + (k_outer_outer * 128)) + (((int)threadIdx.x) * 4)) + 9216))))[0];
+    ((float4*)(placeholder_shared + (((((int)threadIdx.x) * 4) + 1280))))[0] = ((float4*)(placeholder1 + (((((((((int)blockIdx.x) >> 7) * 524288) + ((((int)blockIdx.x) & 31) * 16384)) + (k_outer_outer * 128)) + (((int)threadIdx.x) * 4)) + 10240))))[0];
+    ((float4*)(placeholder_shared + (((((int)threadIdx.x) * 4) + 1408))))[0] = ((float4*)(placeholder1 + (((((((((int)blockIdx.x) >> 7) * 524288) + ((((int)blockIdx.x) & 31) * 16384)) + (k_outer_outer * 128)) + (((int)threadIdx.x) * 4)) + 11264))))[0];
+    ((float4*)(placeholder_shared + (((((int)threadIdx.x) * 4) + 1536))))[0] = ((float4*)(placeholder1 + (((((((((int)blockIdx.x) >> 7) * 524288) + ((((int)blockIdx.x) & 31) * 16384)) + (k_outer_outer * 128)) + (((int)threadIdx.x) * 4)) + 12288))))[0];
+    ((float4*)(placeholder_shared + (((((int)threadIdx.x) * 4) + 1664))))[0] = ((float4*)(placeholder1 + (((((((((int)blockIdx.x) >> 7) * 524288) + ((((int)blockIdx.x) & 31) * 16384)) + (k_outer_outer * 128)) + (((int)threadIdx.x) * 4)) + 13312))))[0];
+    ((float4*)(placeholder_shared + (((((int)threadIdx.x) * 4) + 1792))))[0] = ((float4*)(placeholder1 + (((((((((int)blockIdx.x) >> 7) * 524288) + ((((int)blockIdx.x) & 31) * 16384)) + (k_outer_outer * 128)) + (((int)threadIdx.x) * 4)) + 14336))))[0];
+    ((float4*)(placeholder_shared + (((((int)threadIdx.x) * 4) + 1920))))[0] = ((float4*)(placeholder1 + (((((((((int)blockIdx.x) >> 7) * 524288) + ((((int)blockIdx.x) & 31) * 16384)) + (k_outer_outer * 128)) + (((int)threadIdx.x) * 4)) + 15360))))[0];
+    __syncthreads();
+    for (int k_outer_inner = 0; k_outer_inner < 32; ++k_outer_inner) {
+      T_batch_matmul_NT_local[(0)] = (T_batch_matmul_NT_local[(0)] + (placeholder_d_shared[((((((int)threadIdx.x) >> 4) * 512) + (k_outer_inner * 4)))] * placeholder_shared[((((((int)threadIdx.x) & 15) * 128) + (k_outer_inner * 4)))]));
+      T_batch_matmul_NT_local[(1)] = (T_batch_matmul_NT_local[(1)] + (placeholder_d_shared[(((((((int)threadIdx.x) >> 4) * 512) + (k_outer_inner * 4)) + 128))] * placeholder_shared[((((((int)threadIdx.x) & 15) * 128) + (k_outer_inner * 4)))]));
+      T_batch_matmul_NT_local[(0)] = (T_batch_matmul_NT_local[(0)] + (placeholder_d_shared[(((((((int)threadIdx.x) >> 4) * 512) + (k_outer_inner * 4)) + 1))] * placeholder_shared[(((((((int)threadIdx.x) & 15) * 128) + (k_outer_inner * 4)) + 1))]));
+      T_batch_matmul_NT_local[(1)] = (T_batch_matmul_NT_local[(1)] + (placeholder_d_shared[(((((((int)threadIdx.x) >> 4) * 512) + (k_outer_inner * 4)) + 129))] * placeholder_shared[(((((((int)threadIdx.x) & 15) * 128) + (k_outer_inner * 4)) + 1))]));
+      T_batch_matmul_NT_local[(0)] = (T_batch_matmul_NT_local[(0)] + (placeholder_d_shared[(((((((int)threadIdx.x) >> 4) * 512) + (k_outer_inner * 4)) + 2))] * placeholder_shared[(((((((int)threadIdx.x) & 15) * 128) + (k_outer_inner * 4)) + 2))]));
+      T_batch_matmul_NT_local[(1)] = (T_batch_matmul_NT_local[(1)] + (placeholder_d_shared[(((((((int)threadIdx.x) >> 4) * 512) + (k_outer_inner * 4)) + 130))] * placeholder_shared[(((((((int)threadIdx.x) & 15) * 128) + (k_outer_inner * 4)) + 2))]));
+      T_batch_matmul_NT_local[(0)] = (T_batch_matmul_NT_local[(0)] + (placeholder_d_shared[(((((((int)threadIdx.x) >> 4) * 512) + (k_outer_inner * 4)) + 3))] * placeholder_shared[(((((((int)threadIdx.x) & 15) * 128) + (k_outer_inner * 4)) + 3))]));
+      T_batch_matmul_NT_local[(1)] = (T_batch_matmul_NT_local[(1)] + (placeholder_d_shared[(((((((int)threadIdx.x) >> 4) * 512) + (k_outer_inner * 4)) + 131))] * placeholder_shared[(((((((int)threadIdx.x) & 15) * 128) + (k_outer_inner * 4)) + 3))]));
+      T_batch_matmul_NT_local[(2)] = (T_batch_matmul_NT_local[(2)] + (placeholder_d_shared[(((((((int)threadIdx.x) >> 4) * 512) + (k_outer_inner * 4)) + 256))] * placeholder_shared[((((((int)threadIdx.x) & 15) * 128) + (k_outer_inner * 4)))]));
+      T_batch_matmul_NT_local[(3)] = (T_batch_matmul_NT_local[(3)] + (placeholder_d_shared[(((((((int)threadIdx.x) >> 4) * 512) + (k_outer_inner * 4)) + 384))] * placeholder_shared[((((((int)threadIdx.x) & 15) * 128) + (k_outer_inner * 4)))]));
+      T_batch_matmul_NT_local[(2)] = (T_batch_matmul_NT_local[(2)] + (placeholder_d_shared[(((((((int)threadIdx.x) >> 4) * 512) + (k_outer_inner * 4)) + 257))] * placeholder_shared[(((((((int)threadIdx.x) & 15) * 128) + (k_outer_inner * 4)) + 1))]));
+      T_batch_matmul_NT_local[(3)] = (T_batch_matmul_NT_local[(3)] + (placeholder_d_shared[(((((((int)threadIdx.x) >> 4) * 512) + (k_outer_inner * 4)) + 385))] * placeholder_shared[(((((((int)threadIdx.x) & 15) * 128) + (k_outer_inner * 4)) + 1))]));
+      T_batch_matmul_NT_local[(2)] = (T_batch_matmul_NT_local[(2)] + (placeholder_d_shared[(((((((int)threadIdx.x) >> 4) * 512) + (k_outer_inner * 4)) + 258))] * placeholder_shared[(((((((int)threadIdx.x) & 15) * 128) + (k_outer_inner * 4)) + 2))]));
+      T_batch_matmul_NT_local[(3)] = (T_batch_matmul_NT_local[(3)] + (placeholder_d_shared[(((((((int)threadIdx.x) >> 4) * 512) + (k_outer_inner * 4)) + 386))] * placeholder_shared[(((((((int)threadIdx.x) & 15) * 128) + (k_outer_inner * 4)) + 2))]));
+      T_batch_matmul_NT_local[(2)] = (T_batch_matmul_NT_local[(2)] + (placeholder_d_shared[(((((((int)threadIdx.x) >> 4) * 512) + (k_outer_inner * 4)) + 259))] * placeholder_shared[(((((((int)threadIdx.x) & 15) * 128) + (k_outer_inner * 4)) + 3))]));
+      T_batch_matmul_NT_local[(3)] = (T_batch_matmul_NT_local[(3)] + (placeholder_d_shared[(((((((int)threadIdx.x) >> 4) * 512) + (k_outer_inner * 4)) + 387))] * placeholder_shared[(((((((int)threadIdx.x) & 15) * 128) + (k_outer_inner * 4)) + 3))]));
+    }
+  }
+  for (int i_inner = 0; i_inner < 4; ++i_inner) {
+    T_batch_matmul_NT[(((((((((int)blockIdx.x) >> 5) * 4096) + ((((int)threadIdx.x) >> 4) * 2048)) + (i_inner * 512)) + ((((int)blockIdx.x) & 31) * 16)) + (((int)threadIdx.x) & 15)))] = T_batch_matmul_NT_local[(i_inner)];
+  }
+}
+
--- benchmark/thor/batch_matmul_1024_512.cu
+++ benchmark/thor/batch_matmul_1024_512.cu
@@ -1,5 +1,8 @@
 
 #include <brainstorm/common/cuda_utils.h>
+#include <cmath>
+#include <cublas_v2.h>
+#include <random>
 
 #define uint unsigned int
 #define uchar unsigned char
@@ -133,35 +136,34 @@ __global__ void __launch_bounds__(16)
   for (int k_outer_outer = 0; k_outer_outer < 64; ++k_outer_outer) {
     __syncthreads();
     ((float4*)(placeholder_d_shared + ((((int)threadIdx.x) * 4))))[0] =
-        ((float4*)(placeholder + ((((((((int)blockIdx.x) >> 7) * 1) +
+        ((float4*)(placeholder + ((((((((int)blockIdx.x) >> 7) * 4096) +
                                      ((((int)threadIdx.x) >> 2) * 1024)) +
                                     (k_outer_outer * 16)) +
                                    ((((int)threadIdx.x) & 3) * 4)))))[0];
-    
-    // ((float4*)(placeholder_shared + ((((int)threadIdx.x) * 4))))[0] =
-    //     ((float4*)((placeholder1[(((int)blockIdx.x) >> 7)]) +
-    //                ((((((((int)blockIdx.x) & 127) * 4096)) +
-    //                   ((((int)threadIdx.x) >> 2) * 1024)) +
-    //                  (k_outer_outer * 16)) +
-    //                 ((((int)threadIdx.x) & 3) * 4))))[0];
-    // ((float4*)(placeholder_shared + (((((int)threadIdx.x) * 4) + 64))))[0] =
-    //     ((float4*)(placeholder1[((((int)blockIdx.x) >> 7) + 1)] +
-    //                ((((((((int)blockIdx.x) & 127) * 4096) +
-    //                    ((((int)threadIdx.x) >> 2) * 1024)) +
-    //                   (k_outer_outer * 16)) +
-    //                  ((((int)threadIdx.x) & 3) * 4)))))[0];
-    // ((float4*)(placeholder_shared + (((((int)threadIdx.x) * 4) + 128))))[0] =
-    //     ((float4*)(placeholder1[((((int)blockIdx.x) >> 7) + 2)] +
-    //                ((((((((int)blockIdx.x) & 127) * 4096) +
-    //                    ((((int)threadIdx.x) >> 2) * 1024)) +
-    //                   (k_outer_outer * 16)) +
-    //                  ((((int)threadIdx.x) & 3) * 4)))))[0];
-    // ((float4*)(placeholder_shared + (((((int)threadIdx.x) * 4) + 192))))[0] =
-    //     ((float4*)(placeholder1[((((int)blockIdx.x) >> 7) + 3)] +
-    //                ((((((((int)blockIdx.x) & 127) * 4096) +
-    //                    ((((int)threadIdx.x) >> 2) * 1024)) +
-    //                   (k_outer_outer * 16)) +
-    //                  ((((int)threadIdx.x) & 3) * 4)))))[0];
+    ((float4*)(placeholder_shared + ((((int)threadIdx.x) * 4))))[0] =
+        ((float4*)((placeholder1[((((int)blockIdx.x) >> 7) * 4)]) +
+                   ((((((((int)blockIdx.x) & 127) * 4096)) +
+                      ((((int)threadIdx.x) >> 2) * 1024)) +
+                     (k_outer_outer * 16)) +
+                    ((((int)threadIdx.x) & 3) * 4))))[0];
+    ((float4*)(placeholder_shared + (((((int)threadIdx.x) * 4) + 64))))[0] =
+        ((float4*)(placeholder1[(((((int)blockIdx.x) >> 7) * 4) + 1)] +
+                   ((((((((int)blockIdx.x) & 127) * 4096) +
+                       ((((int)threadIdx.x) >> 2) * 1024)) +
+                      (k_outer_outer * 16)) +
+                     ((((int)threadIdx.x) & 3) * 4)))))[0];
+    ((float4*)(placeholder_shared + (((((int)threadIdx.x) * 4) + 128))))[0] =
+        ((float4*)(placeholder1[(((((int)blockIdx.x) >> 7) * 4) + 2)] +
+                   ((((((((int)blockIdx.x) & 127) * 4096) +
+                       ((((int)threadIdx.x) >> 2) * 1024)) +
+                      (k_outer_outer * 16)) +
+                     ((((int)threadIdx.x) & 3) * 4)))))[0];
+    ((float4*)(placeholder_shared + (((((int)threadIdx.x) * 4) + 192))))[0] =
+        ((float4*)(placeholder1[(((((int)blockIdx.x) >> 7) * 4) + 3)] +
+                   ((((((((int)blockIdx.x) & 127) * 4096) +
+                       ((((int)threadIdx.x) >> 2) * 1024)) +
+                      (k_outer_outer * 16)) +
+                     ((((int)threadIdx.x) & 3) * 4)))))[0];
     __syncthreads();
     T_batch_matmul_NT_local[(0)] =
         (T_batch_matmul_NT_local[(0)] +
@@ -249,11 +251,39 @@ void init_with_rand(float* data, int size) {
 
 bool check_results(float* lfs, float* rhs, int size) {
   for (int i = 0; i < size; ++i) {
-    if (lfs[i] != rhs[i]) {
+    if (std::abs(lfs[i] - rhs[i]) > 1e-5) {
+      printf("%d-th value not equal:  %f, %f\n", i, lfs[i], rhs[i]);
       return false;
     }
   }
   return true;
+}
+
+__global__ void __launch_bounds__(32)
+    pointer_array_assign(float** dst, float* src, int index[]) {
+  dst[((blockIdx.x * 32) + threadIdx.x)] =
+      src + index[((blockIdx.x * 32) + threadIdx.x)] * 1024 * 512;
+}
+
+__global__ void __launch_bounds__(1024)
+    array_value_comp(float** dst, float* src) {
+  if (dst[blockIdx.y][blockIdx.x * 1024 + threadIdx.x] !=
+      src[blockIdx.y * 524288 + blockIdx.x * 1024 + threadIdx.x]) {
+    printf("%d-th value not equal:  %f, %f\n",
+           blockIdx.y * 524288 + blockIdx.x * 1024 + threadIdx.x,
+           dst[blockIdx.y][blockIdx.x * 1024 + threadIdx.x],
+           src[blockIdx.y * 524288 + blockIdx.x * 1024 + threadIdx.x]);
+  }
+  __syncthreads();
+}
+
+void generate_expert_index(int* index, int size, int expert_num) {
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_int_distribution<> dis(0, expert_num - 1);
+  for (int i = 0; i < size; ++i) {
+    index[i] = dis(gen);
+  }
 }
 
 int main(int argc, char const* argv[]) {
@@ -268,6 +298,9 @@ int main(int argc, char const* argv[]) {
   // create stream
   cudaStream_t stream;
   CUDA_CHECK(cudaStreamCreate(&stream));
+  cublasHandle_t cublas_handle;
+  CUBLAS_CHECK(cublasCreate(&cublas_handle));
+  CUBLAS_CHECK(cublasSetStream(cublas_handle, stream));
 
   // create CUDA events
   cudaEvent_t start, stop;
@@ -281,9 +314,12 @@ int main(int argc, char const* argv[]) {
   CUDA_CHECK(cudaMallocHost((void**)&C_h, size_C * sizeof(float)));
   CUDA_CHECK(cudaMallocHost((void**)&C_h_grand, size_C * sizeof(float)));
 
-  float *A_d, *B_d, *C_d, *C_d_grand;
+  float *A_d, *B_d, *B_d_linear, *C_d, *C_d_grand;
+  float** B_d_array;
   CUDA_CHECK(cudaMalloc((void**)&A_d, size_A * sizeof(float)));
   CUDA_CHECK(cudaMalloc((void**)&B_d, size_B * sizeof(float)));
+  CUDA_CHECK(cudaMalloc((void**)&B_d_linear, size_B * sizeof(float)));
+  CUDA_CHECK(cudaMalloc((void**)&B_d_array, sizeof(float*) * 64));
   CUDA_CHECK(cudaMalloc((void**)&C_d, size_C * sizeof(float)));
   CUDA_CHECK(cudaMalloc((void**)&C_d_grand, size_C * sizeof(float)));
 
@@ -291,32 +327,89 @@ int main(int argc, char const* argv[]) {
   init_with_rand(B_h, size_B);
   init_with_val(C_h, size_C, 0.0f);
 
-  float* B_d_array[64];
-  for (int i = 0; i < 64; ++i) {
-    B_d_array[i] = B_d + i * 1024 * 512;
-  }
-
   CUDA_CHECK(
       cudaMemcpy(A_d, A_h, size_A * sizeof(float), cudaMemcpyHostToDevice));
   CUDA_CHECK(
       cudaMemcpy(B_d, B_h, size_B * sizeof(float), cudaMemcpyHostToDevice));
 
-  dim3 threads_per_block(16);
-  dim3 blocks_per_grid(256);
+  int* expert_indexes;
+  CUDA_CHECK(cudaMallocHost((void**)&expert_indexes, sizeof(int) * 64));
+  generate_expert_index(expert_indexes, 64, 1);
 
-  default_batch_matmul<<<blocks_per_grid, threads_per_block, 0, stream>>>(
-      A_d, B_d, C_d_grand);
-  CUDA_CHECK(cudaGetLastError());
+  for (auto i = 0; i < 64; ++i) {
+    CUDA_CHECK(cudaMemcpy(
+        B_d_linear + i * 1024 * 512, B_d + expert_indexes[i] * 1024 * 512,
+        1024 * 512 * sizeof(float), cudaMemcpyDeviceToDevice));
+  }
   CUDA_CHECK(cudaDeviceSynchronize());
-  expert_batch_matmul<<<blocks_per_grid, threads_per_block, 0, stream>>>(
-      A_d, B_d_array, C_d);
-  CUDA_CHECK(cudaGetLastError());
+
+  pointer_array_assign<<<2, 32>>>(B_d_array, B_d, expert_indexes);
   CUDA_CHECK(cudaDeviceSynchronize());
+
+  dim3 grids(512, 64);
+  dim3 blocks(1024, 1);
+
+  array_value_comp<<<grids, blocks>>>(B_d_array, B_d_linear);
+  CUDA_CHECK(cudaDeviceSynchronize());
+
+  dim3 threads_per_block(4 * 4);
+  dim3 blocks_per_grid(512 * 64 / threads_per_block.x);
+  float alpha = 1.0f;
+  float beta = 0.0f;
+  // warm up
+  for (auto i = 0; i < 1024; ++i) {
+    CUBLAS_CHECK(cublasSgemm(cublas_handle, CUBLAS_OP_N, CUBLAS_OP_T, 64, 512,
+                             1024, &alpha, A_d, 1024, B_d_linear, 512, &beta,
+                             C_d, 512));
+    default_batch_matmul<<<blocks_per_grid, threads_per_block, 0, stream>>>(
+        A_d, B_d_linear, C_d_grand);
+    expert_batch_matmul<<<blocks_per_grid, threads_per_block, 0, stream>>>(
+        A_d, B_d_array, C_d);
+  }
+  CUDA_CHECK(cudaDeviceSynchronize());
+
+  // start timing
+  int test_num = 1000;
+  CUDA_CHECK(cudaEventRecord(start, stream));
+  for (auto i = 0; i < test_num; ++i) {
+    CUBLAS_CHECK(cublasSgemm(cublas_handle, CUBLAS_OP_N, CUBLAS_OP_T, 64, 512,
+                             1024, &alpha, A_d, 1024, B_d_linear, 512, &beta,
+                             C_d, 512));
+  }
+  CUDA_CHECK(cudaEventRecord(stop, stream));
+  CUDA_CHECK(cudaEventSynchronize(stop));
+  CUDA_CHECK(cudaEventElapsedTime(&elapsed_time, start, stop));
+  printf("default cublas matmul %f ms\n", elapsed_time / test_num);
+
+  CUDA_CHECK(cudaEventRecord(start, stream));
+  for (auto i = 0; i < test_num; ++i) {
+    default_batch_matmul<<<blocks_per_grid, threads_per_block, 0, stream>>>(
+        A_d, B_d_linear, C_d_grand);
+  }
+  CUDA_CHECK(cudaEventRecord(stop, stream));
+  CUDA_CHECK(cudaEventSynchronize(stop));
+  CUDA_CHECK(cudaEventElapsedTime(&elapsed_time, start, stop));
+  printf("default batch matmul %f ms\n", elapsed_time / test_num);
+
+  CUDA_CHECK(cudaEventRecord(start, stream));
+  for (auto i = 0; i < test_num; ++i) {
+    expert_batch_matmul<<<blocks_per_grid, threads_per_block, 0, stream>>>(
+        A_d, B_d_array, C_d);
+  }
+  CUDA_CHECK(cudaEventRecord(stop, stream));
+  CUDA_CHECK(cudaEventSynchronize(stop));
+  CUDA_CHECK(cudaEventElapsedTime(&elapsed_time, start, stop));
+  printf("expert batch matmul %f ms\n", elapsed_time / test_num);
+
   CUDA_CHECK(cudaMemcpy(C_h_grand, C_d_grand, size_C * sizeof(float),
                         cudaMemcpyDeviceToHost));
   CUDA_CHECK(
       cudaMemcpy(C_h, C_d, size_C * sizeof(float), cudaMemcpyDeviceToHost));
-  check_results(C_h_grand, C_h, size_C);
-
+  bool if_equal = check_results(C_h_grand, C_h, size_C);
+  if (if_equal) {
+    printf("passed\n");
+  } else {
+    printf("failed\n");
+  }
   return 0;
 }
