1bd79d2d5ee51aa357292d48762d279655f3b280
Raphael-Hao <raphaelhao@outlook.com>
2022-03-12 23:00:37 +0800

    update

---
+++ sample/brainstorm.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# Motto: Were It to Benefit My Country, I Would Lay Down My Life!
+# \file: /brainstorm.py
+# \brief:
+# Author: raphael hao
+from typing import List, Optional, Union
+
+import torch.nn as nn
+import torch
+
+
+class shutter(nn.Module):
+    def __init__(self) -> None:
+        self.token_grain = 16
+
+    def forward(self, x):
+        return [x for _ in range(self.token_grain)]
+
+
+class SampleDNN(nn.Module):
+    def __init__(self):
+        self.linear1 = nn.Linear(784, 256)
+        self.linear2 = nn.Linear(256, 10)
+        self.relu = nn.ReLU()
+        self.shutter = shutter()
+
+    def forward(self, x):
+        x = self.linear1(x)
+        x = self.relu(x)
+        x = self.linear2(x)
+
+        return x
+
+
+x: Union[torch.Tensor, List[torch.Tensor]]
+Linear: nn.Module
+ScatterRouter: nn.Module
+GatherRouter: nn.Module
+Shutter: nn.Module
+Netlet: nn.Module
+RandomRouter: nn.Module
+CandidateModules: nn.ModuleList
+DistScatterRouter: nn.Module
+DistGatherRouter: nn.Module
+RecursiveRouter: nn.Module
+FusionNetlet: nn.Module
+
+x = Linear(x)
+x = Shutter(x)
+x = RandomRouter(x, CandidateModules)
+x = Linear(x)
+
+# transform to our IR
+x = Linear(x)
+x = Shutter(x)
+x, y = ScatterRouter(x) # x the scatter result, y the reverse indices
+x = Netlet(x)
+x = GatherRouter(x, y)
+x = Linear(x)
+
+# determine single or distribute acording to hint
+x = Linear(x)
+x = Shutter(x)
+x, y = DistScatterRouter(x)
+x = Netlet(x)
+x = DistGatherRouter(x, y)
+
+# horizontal fusion
+x = Linear(x)
+x = Shutter(x)
+x, y = DistScatterRouter(x)
+x = FusionNetlet(x)
+x = DistGatherRouter(x, y)
+
+with RecursiveRouter(x):
+    x = FusionNetlet(x)
--- test/kernel_test.cu
+++ test/kernel_test.cu
@@ -119,9 +119,9 @@ __global__ void __launch_bounds__(16)
 }
 
 __global__ void __launch_bounds__(128)
-    matmul_1_1024_512(float* __restrict__ placeholder,
-                      float* __restrict__ placeholder1,
-                      float* __restrict__ T_batch_matmul_NT) {
+    matmul_1_1024_512_1(float* __restrict__ placeholder,
+                        float* __restrict__ placeholder1,
+                        float* __restrict__ T_batch_matmul_NT) {
   float T_batch_matmul_NT_local[1];
   __shared__ float placeholder_d_shared[512];
   __shared__ float placeholder_shared[8192];
@@ -158,6 +158,120 @@ __global__ void __launch_bounds__(128)
   T_batch_matmul_NT[(
       ((((((int)blockIdx.x) >> 2) * 512) + ((((int)blockIdx.x) & 3) * 128)) +
        ((int)threadIdx.x)))] = T_batch_matmul_NT_local[(0)];
+}
+
+__global__ void __launch_bounds__(16)
+    matmul_1_1024_512(float* __restrict__ placeholder,
+                      float* __restrict__ placeholder1,
+                      float* __restrict__ T_batch_matmul_NT) {
+  float T_batch_matmul_NT_local[1];
+  __shared__ float placeholder_d_shared[64];
+  __shared__ float placeholder_shared[1024];
+  T_batch_matmul_NT_local[(0)] = 0.000000e+00f;
+  for (int k_outer_outer = 0; k_outer_outer < 16; ++k_outer_outer) {
+    __syncthreads();
+    ((float2*)(placeholder_d_shared + ((((int)threadIdx.x) * 2))))[0] = ((
+        float2*)(placeholder +
+                 (((((((int)blockIdx.x) >> 5) * 1024) + (k_outer_outer * 64)) +
+                   (((int)threadIdx.x) * 2)))))[0];
+    ((float2*)(placeholder_d_shared + (((((int)threadIdx.x) * 2) + 32))))[0] =
+        ((float2*)(placeholder + ((((((((int)blockIdx.x) >> 5) * 1024) +
+                                     (k_outer_outer * 64)) +
+                                    (((int)threadIdx.x) * 2)) +
+                                   32))))[0];
+    ((float4*)(placeholder_shared + ((((int)threadIdx.x) * 4))))[0] =
+        ((float4*)(placeholder1 +
+                   ((((((int)blockIdx.x) * 16384) + (k_outer_outer * 64)) +
+                     (((int)threadIdx.x) * 4)))))[0];
+    ((float4*)(placeholder_shared + (((((int)threadIdx.x) * 4) + 64))))[0] =
+        ((float4*)(placeholder1 +
+                   (((((((int)blockIdx.x) * 16384) + (k_outer_outer * 64)) +
+                      (((int)threadIdx.x) * 4)) +
+                     1024))))[0];
+    ((float4*)(placeholder_shared + (((((int)threadIdx.x) * 4) + 128))))[0] =
+        ((float4*)(placeholder1 +
+                   (((((((int)blockIdx.x) * 16384) + (k_outer_outer * 64)) +
+                      (((int)threadIdx.x) * 4)) +
+                     2048))))[0];
+    ((float4*)(placeholder_shared + (((((int)threadIdx.x) * 4) + 192))))[0] =
+        ((float4*)(placeholder1 +
+                   (((((((int)blockIdx.x) * 16384) + (k_outer_outer * 64)) +
+                      (((int)threadIdx.x) * 4)) +
+                     3072))))[0];
+    ((float4*)(placeholder_shared + (((((int)threadIdx.x) * 4) + 256))))[0] =
+        ((float4*)(placeholder1 +
+                   (((((((int)blockIdx.x) * 16384) + (k_outer_outer * 64)) +
+                      (((int)threadIdx.x) * 4)) +
+                     4096))))[0];
+    ((float4*)(placeholder_shared + (((((int)threadIdx.x) * 4) + 320))))[0] =
+        ((float4*)(placeholder1 +
+                   (((((((int)blockIdx.x) * 16384) + (k_outer_outer * 64)) +
+                      (((int)threadIdx.x) * 4)) +
+                     5120))))[0];
+    ((float4*)(placeholder_shared + (((((int)threadIdx.x) * 4) + 384))))[0] =
+        ((float4*)(placeholder1 +
+                   (((((((int)blockIdx.x) * 16384) + (k_outer_outer * 64)) +
+                      (((int)threadIdx.x) * 4)) +
+                     6144))))[0];
+    ((float4*)(placeholder_shared + (((((int)threadIdx.x) * 4) + 448))))[0] =
+        ((float4*)(placeholder1 +
+                   (((((((int)blockIdx.x) * 16384) + (k_outer_outer * 64)) +
+                      (((int)threadIdx.x) * 4)) +
+                     7168))))[0];
+    ((float4*)(placeholder_shared + (((((int)threadIdx.x) * 4) + 512))))[0] =
+        ((float4*)(placeholder1 +
+                   (((((((int)blockIdx.x) * 16384) + (k_outer_outer * 64)) +
+                      (((int)threadIdx.x) * 4)) +
+                     8192))))[0];
+    ((float4*)(placeholder_shared + (((((int)threadIdx.x) * 4) + 576))))[0] =
+        ((float4*)(placeholder1 +
+                   (((((((int)blockIdx.x) * 16384) + (k_outer_outer * 64)) +
+                      (((int)threadIdx.x) * 4)) +
+                     9216))))[0];
+    ((float4*)(placeholder_shared + (((((int)threadIdx.x) * 4) + 640))))[0] =
+        ((float4*)(placeholder1 +
+                   (((((((int)blockIdx.x) * 16384) + (k_outer_outer * 64)) +
+                      (((int)threadIdx.x) * 4)) +
+                     10240))))[0];
+    ((float4*)(placeholder_shared + (((((int)threadIdx.x) * 4) + 704))))[0] =
+        ((float4*)(placeholder1 +
+                   (((((((int)blockIdx.x) * 16384) + (k_outer_outer * 64)) +
+                      (((int)threadIdx.x) * 4)) +
+                     11264))))[0];
+    ((float4*)(placeholder_shared + (((((int)threadIdx.x) * 4) + 768))))[0] =
+        ((float4*)(placeholder1 +
+                   (((((((int)blockIdx.x) * 16384) + (k_outer_outer * 64)) +
+                      (((int)threadIdx.x) * 4)) +
+                     12288))))[0];
+    ((float4*)(placeholder_shared + (((((int)threadIdx.x) * 4) + 832))))[0] =
+        ((float4*)(placeholder1 +
+                   (((((((int)blockIdx.x) * 16384) + (k_outer_outer * 64)) +
+                      (((int)threadIdx.x) * 4)) +
+                     13312))))[0];
+    ((float4*)(placeholder_shared + (((((int)threadIdx.x) * 4) + 896))))[0] =
+        ((float4*)(placeholder1 +
+                   (((((((int)blockIdx.x) * 16384) + (k_outer_outer * 64)) +
+                      (((int)threadIdx.x) * 4)) +
+                     14336))))[0];
+    ((float4*)(placeholder_shared + (((((int)threadIdx.x) * 4) + 960))))[0] =
+        ((float4*)(placeholder1 +
+                   (((((((int)blockIdx.x) * 16384) + (k_outer_outer * 64)) +
+                      (((int)threadIdx.x) * 4)) +
+                     15360))))[0];
+    __syncthreads();
+    for (int k_outer_inner = 0; k_outer_inner < 2; ++k_outer_inner) {
+      for (int k_inner = 0; k_inner < 32; ++k_inner) {
+        T_batch_matmul_NT_local[(0)] =
+            (T_batch_matmul_NT_local[(0)] +
+             (placeholder_d_shared[(((k_outer_inner * 32) + k_inner))] *
+              placeholder_shared[(
+                  (((((int)threadIdx.x) * 64) + (k_outer_inner * 32)) +
+                   k_inner))]));
+      }
+    }
+  }
+  T_batch_matmul_NT[(((((int)blockIdx.x) * 16) + ((int)threadIdx.x)))] =
+      T_batch_matmul_NT_local[(0)];
 }
 }
 
@@ -230,8 +344,8 @@ int main(int argc, char const* argv[]) {
 
   dim3 threads_per_block(4 * 4);
   dim3 blocks_per_grid(512 * 64 / threads_per_block.x);
-  dim3 matmul_block_size(128);
-  dim3 matmul_grid_size(256);
+  dim3 matmul_block_size(16);   //(128);
+  dim3 matmul_grid_size(2048);  //(256);
 
   // warm up
   for (auto i = 0; i < 1024; ++i) {
