1a1fbd80a5aada163224f6b3b6dc46df182fa2fa
Raphael-Hao <raphaelhao@outlook.com>
2022-03-28 16:08:58 +0800

    update dynamic routing

--- 3rdparty/dynamic_routing/playground/Dynamic/Seg.Layer16.SmallGate.Dynamic_A/config.py
+++ 3rdparty/dynamic_routing/playground/Dynamic/Seg.Layer16.SmallGate.Dynamic_A/config.py
@@ -4,10 +4,10 @@ from dl_lib.configs.segm_config import SemanticSegmentationConfig
 
 _config_dict = dict(
     MODEL=dict(
-        WEIGHTS="",
+        WEIGHTS="/home/whcui/checkpoints/",
         CAL_FLOPS=True,
         BACKBONE=dict(
-            CELL_TYPE=['sep_conv_3x3', 'skip_connect'],
+            CELL_TYPE=["sep_conv_3x3", "skip_connect"],
             LAYER_NUM=16,
             CELL_NUM_LIST=[2, 3, 4] + [4 for _ in range(13)],
             INIT_CHANNEL=64,
@@ -22,7 +22,7 @@ _config_dict = dict(
             SMALL_GATE=True,
         ),
         SEM_SEG_HEAD=dict(
-            IN_FEATURES=['layer_0', 'layer_1', 'layer_2', 'layer_3'],
+            IN_FEATURES=["layer_0", "layer_1", "layer_2", "layer_3"],
             NUM_CLASSES=19,
             IGNORE_VALUE=255,
             NORM="nnSyncBN",
@@ -38,8 +38,8 @@ _config_dict = dict(
         ),
     ),
     DATASETS=dict(
-        TRAIN=("cityscapes_fine_sem_seg_train", ),
-        TEST=("cityscapes_fine_sem_seg_val", ),
+        TRAIN=("cityscapes_fine_sem_seg_train",),
+        TEST=("cityscapes_fine_sem_seg_val",),
     ),
     SOLVER=dict(
         LR_SCHEDULER=dict(
@@ -47,24 +47,36 @@ _config_dict = dict(
             POLY_POWER=0.9,
             MAX_ITER=190000,
         ),
-        OPTIMIZER=dict(BASE_LR=0.05, ),
+        OPTIMIZER=dict(
+            BASE_LR=0.05,
+        ),
         IMS_PER_BATCH=8,
         CHECKPOINT_PERIOD=5000,
         GRAD_CLIP=5.0,
     ),
     INPUT=dict(
-        MIN_SIZE_TRAIN=(512, 768, 1024, 1280, 1536, 2048, ),
+        MIN_SIZE_TRAIN=(
+            512,
+            768,
+            1024,
+            1280,
+            1536,
+            2048,
+        ),
         MIN_SIZE_TRAIN_SAMPLING="choice",
         MAX_SIZE_TRAIN=4096,
         MIN_SIZE_TEST=1024,
         MAX_SIZE_TEST=2048,
         # FIX_SIZE_FOR_FLOPS=[768, 768],
         FIX_SIZE_FOR_FLOPS=[1024, 2048],
-        CROP_PAD=dict(SIZE=[768, 768], ),
+        CROP_PAD=dict(
+            SIZE=[768, 768],
+        ),
     ),
     OUTPUT_DIR=osp.join(
-        '/data/Outputs/model_logs/dl_lib_playground',
-        osp.split(osp.realpath(__file__))[0].split("playground/")[-1]),
+        "/data/Outputs/model_logs/dl_lib_playground",
+        osp.split(osp.realpath(__file__))[0].split("playground/")[-1],
+    ),
 )
 
 
--- 3rdparty/dynamic_routing/playground/Dynamic/Seg.Layer33.ImageNet/config.py
+++ 3rdparty/dynamic_routing/playground/Dynamic/Seg.Layer33.ImageNet/config.py
@@ -7,7 +7,7 @@ _config_dict = dict(
         WEIGHTS="/data/dl_lib_modelzoo/dynamicmodel/Dynamic-L33B4-A74-convert-seg.pth",
         CAL_FLOPS=True,
         BACKBONE=dict(
-            CELL_TYPE=['sep_conv_3x3', 'skip_connect'],
+            CELL_TYPE=["sep_conv_3x3", "skip_connect"],
             LAYER_NUM=33,
             CELL_NUM_LIST=[2, 3, 4] + [4 for _ in range(30)],
             INIT_CHANNEL=64,
@@ -22,7 +22,7 @@ _config_dict = dict(
             SMALL_GATE=False,
         ),
         SEM_SEG_HEAD=dict(
-            IN_FEATURES=['layer_0', 'layer_1', 'layer_2', 'layer_3'],
+            IN_FEATURES=["layer_0", "layer_1", "layer_2", "layer_3"],
             NUM_CLASSES=19,
             IGNORE_VALUE=255,
             NORM="nnSyncBN",
@@ -38,8 +38,8 @@ _config_dict = dict(
         ),
     ),
     DATASETS=dict(
-        TRAIN=("cityscapes_fine_sem_seg_train", ),
-        TEST=("cityscapes_fine_sem_seg_val", ),
+        TRAIN=("cityscapes_fine_sem_seg_train",),
+        TEST=("cityscapes_fine_sem_seg_val",),
     ),
     SOLVER=dict(
         LR_SCHEDULER=dict(
@@ -56,27 +56,44 @@ _config_dict = dict(
         GRAD_CLIP=5.0,
     ),
     INPUT=dict(
-        MIN_SIZE_TRAIN=(512, 768, 1024, 1280, 1536, 2048, ),
+        MIN_SIZE_TRAIN=(
+            512,
+            768,
+            1024,
+            1280,
+            1536,
+            2048,
+        ),
         MIN_SIZE_TRAIN_SAMPLING="choice",
         MAX_SIZE_TRAIN=4096,
         MIN_SIZE_TEST=1024,
         MAX_SIZE_TEST=2048,
         # FIX_SIZE_FOR_FLOPS=[768,768],
         FIX_SIZE_FOR_FLOPS=[1024, 2048],
-        CROP_PAD=dict(SIZE=[768, 768], ),
+        CROP_PAD=dict(
+            SIZE=[768, 768],
+        ),
     ),
     TEST=dict(
         AUG=dict(
             ENABLED=False,
-            MIN_SIZES=(512, 768, 1024, 1280, 1536, 2048, ),
+            MIN_SIZES=(
+                512,
+                768,
+                1024,
+                1280,
+                1536,
+                2048,
+            ),
             MAX_SIZE=4096,
             FLIP=True,
         ),
         PRECISE_BN=dict(ENABLED=True),
     ),
     OUTPUT_DIR=osp.join(
-        '/data/Outputs/model_logs/dl_lib_playground',
-        osp.split(osp.realpath(__file__))[0].split("playground/")[-1]),
+        "/data/Outputs/model_logs/dl_lib_playground",
+        osp.split(osp.realpath(__file__))[0].split("playground/")[-1],
+    ),
 )
 
 
