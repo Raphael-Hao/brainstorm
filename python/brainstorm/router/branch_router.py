2a2871066af95e2ccbf1152de9be925853d37856
Raphael-Hao <raphaelhao@outlook.com>
2022-04-07 11:31:25 +0800

    add base router and three routers

---
+++ python/brainstorm/router/branch_router.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# Licensed under the MIT license.
+# \file: /branch_router.py
+# \brief: 
+# Author: raphael hao
+
--- python/brainstorm/router/thor_moe.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding:utf-8 -*-
-# Motto: Were It to Benefit My Country, I Would Lay Down My Life!
-# \file: /thor_moe.py
-# \brief: MoE layer for thor
-# Author: raphael hao
-
