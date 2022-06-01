ae4c6b87959141bacf8d1369fbbe8993a8a4a1a5
Raphael-Hao <raphaelhao@outlook.com>
2022-06-01 19:00:38 +0800

    [primitive] stablize the scatter and gather router

--- python/brt/router/base.py
+++ python/brt/router/base.py
@@ -1,10 +1,13 @@
 # Copyright (c) 2022 by Microsoft Corporation.
 # Licensed under the MIT license.
 
-from typing import List, Union
+from typing import List, Tuple, Union
 
+import torch
 import torch.nn as nn
 from brt.primitive import router
+
+from .symbolic import symbolic_tag_route
 
 
 @router
@@ -25,11 +28,37 @@ class BaseRouter(nn.Module):
         self.gran_dim = gran_dim
         self.active_counter = 0
 
+    # def __init_subclass__(cls):
+    #     if getattr(cls, "_traced", False) is False:
+    #         cls = router(cls)
+
     def route(self, *inputs):
         raise NotImplementedError
 
     def symbolic_route(self, *inputs):
         raise NotImplementedError
 
-    def record(self):
-        raise NotImplementedError
+
+@router
+class TagRouter(BaseRouter):
+    def __init__(self, gran_dim: Union[int, List[int]]):
+        super().__init__(route_num=1, gran_dim=gran_dim)
+
+    def route(
+        self, inputs: torch.Tensor
+    ) -> Tuple[Tuple[torch.Tensor, torch.Tensor], int]:
+        loads = inputs.size(0)
+        indices = torch.arange(0, loads, dtype=torch.long, device=inputs.device)
+        return inputs, indices, loads
+
+    def symbolic_route(self, inputs: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
+        return symbolic_tag_route(inputs)
+
+
+class SparseRouter(BaseRouter):
+    def __init__(self, gran_dim: Union[int, List[int]]):
+        super().__init__(route_num=1, gran_dim=gran_dim)
+
+    def route(self, inputs: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
+
+        return inputs
--- python/brt/router/symbolic.pyi
+++ python/brt/router/symbolic.pyi
@@ -2,7 +2,7 @@ from typing import List, Tuple
 
 import torch
 
-
+def symbolic_tag_route(inputs: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]: ...
 def symbolic_scatter_route(
     inputs: torch.Tensor, router_kind: int, route_num: int
 ) -> Tuple[List[torch.Tensor], List[torch.Tensor], int]: ...
