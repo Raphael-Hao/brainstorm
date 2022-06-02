e6594bbbe667c1480d67745d8d8035a6961d83c1
Raphael-Hao <raphaelhao@outlook.com>
2022-06-02 23:04:32 +0800

    [router] sparse router added without check

--- python/brt/router/dispatcher/default_dispatcher.py
+++ python/brt/router/dispatcher/default_dispatcher.py
@@ -19,7 +19,11 @@ class DefaultDispatcher(Dispatcher):
         super().__init__(route_num, transform, reduction)
 
     def dispatch(
-        self, inputs: torch.Tensor, route_indices: torch.Tensor, gates: torch.Tensor
+        self,
+        inputs: torch.Tensor,
+        route_indices: torch.Tensor,
+        gates: torch.Tensor,
+        tags: torch.Tensor = None,
     ):
         """
         Dispatch the inputs into the the list of torch.Tensor with indices
@@ -33,24 +37,26 @@ class DefaultDispatcher(Dispatcher):
             torch.zeros(0, 1, dtype=torch.int64, device=inputs.device)
         ] * self.route_num
         for i in range(self.route_num):
-            tags = torch.nonzero(route_indices[:, i].view(-1)).to(inputs.device)
-            if tags.numel() > 0:
-                route_tags[i] = tags
-                tags = tags.repeat(1, route_size).view(-1, *route_shape)
+            indices = torch.nonzero(route_indices[:, i].view(-1)).to(inputs.device)
+            if indices.numel() > 0:
+                route_tags[i] = (
+                    torch.gather(tags, 0, indices) if tags is not None else indices
+                )
+                indices = indices.repeat(1, route_size).view(-1, *route_shape)
                 if self.transform:
                     gate = gates[:, i].reshape(
                         (inputs.size(0),) + (1,) * len(route_shape)
                     )
-                    route_results[i] = torch.gather(inputs * gate, 0, tags)
+                    route_results[i] = torch.gather(inputs * gate, 0, indices)
                 else:
-                    route_results[i] = torch.gather(inputs, 0, tags)
+                    route_results[i] = torch.gather(inputs, 0, indices)
         return route_results, route_tags
 
     def combine(
         self,
         inputs: List[torch.Tensor],
         tags: List[torch.Tensor],
-        loads: int,
+        loads: int = 0,
     ) -> torch.Tensor:
         """
         Combine the outputs of the routers into the final outputs
@@ -58,23 +64,23 @@ class DefaultDispatcher(Dispatcher):
         assert len(inputs) == self.route_num and len(tags) == self.route_num
         route_shape = list(inputs[0].shape[1:])
         inputs = torch.cat(inputs, dim=0)
-        tags = (
-            torch.cat(tags, dim=0)
-            .repeat(1, np.prod(route_shape))
-            .view(-1, *route_shape)
-        )
+        tags = torch.cat(tags, dim=0)
         if inputs.numel() == 0:
             route_results = torch.zeros(
                 0, *route_shape, dtype=inputs.dtype, device=inputs.device
             )
+            return route_results, tags
+        if loads == 0:
+            tags, inverse = torch.unique(tags, sorted=True, return_inverse=True)
+            route_results = torch.scatter_reduce(inputs, 0, inverse, self.reduction)
         else:
-            route_results = torch.zeros(
-                loads, *route_shape, dtype=inputs.dtype, device=inputs.device
-            )
-            if self.reduction == "add":
-                route_results = torch.scatter_add(route_results, 0, tags, inputs)
-            else:
-                raise NotImplementedError(
-                    f"Reduction method {self.reduction} is not supported"
+            tags = tags.repeat(1, np.prod(route_shape)).view(-1, *route_shape)
+            if inputs.numel() == 0:
+                route_results = torch.zeros(
+                    0, *route_shape, dtype=inputs.dtype, device=inputs.device
                 )
-        return route_results
+            else:
+                route_results = torch.scatter_reduce(
+                    inputs, 0, tags, self.reduction, loads
+                )
+        return route_results, tags
