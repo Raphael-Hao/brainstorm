94ea5839cfa12825abda2ea76c6955fdfb43efe5
Raphael-Hao <raphaelhao@outlook.com>
2022-06-04 18:06:28 +0800

    [router] add FlowTensor abstraction to unify the data, tag, and load

--- python/brt/router/dispatcher/default_dispatcher.py
+++ python/brt/router/dispatcher/default_dispatcher.py
@@ -1,10 +1,11 @@
 # Copyright (c) 2022 by Microsoft Corporation.
 # Licensed under the MIT license.
 
-from typing import List
+from typing import List, Union
 
 import numpy as np
 import torch
+from brt.router.flow_tensor import FlowTensor
 
 from .base import Dispatcher
 
@@ -15,78 +16,86 @@ class DefaultDispatcher(Dispatcher):
     The default dispatcher will dispatch the inputs to the routers just according to the ground-truth routing indices.
     """
 
-    def __init__(self, route_num, transform=True, reduction="add"):
+    def __init__(self, route_num, transform=True, reduction="add", sparse=True):
         """currently we only support add due to the scatter_reduce of torch
         is in development and will be released in 1.12, After that we will
         support "sum", "prod", "mean", "amax", "amin, see details in
         https://github.com/pytorch/pytorch/issues/74770
         """
         super().__init__(route_num, transform, reduction)
+        self.sparse = sparse
 
     def dispatch(
         self,
-        inputs: torch.Tensor,
+        inputs: FlowTensor,
         route_indices: torch.Tensor,
         gates: torch.Tensor,
-        tags: torch.Tensor = None,
-    ):
+    ) -> List[FlowTensor]:
         """
         Dispatch the inputs into the the list of torch.Tensor with indices
         """
-        route_shape = list(inputs.shape[1:])
+        route_data = inputs.data
+        route_tag = inputs.tag
+        load = inputs.load
+        route_shape = list(route_data.shape[1:])
         route_size = np.prod(route_shape)
-        route_results = [
-            torch.zeros(0, *route_shape, dtype=inputs.dtype, device=inputs.device)
-        ] * self.route_num
-        route_tags = [
-            torch.zeros(0, 1, dtype=torch.int64, device=inputs.device)
-        ] * self.route_num
+
+        results = [
+            FlowTensor(
+                data=torch.zeros(
+                    0, *route_shape, dtype=route_data.dtype, device=route_data.device
+                ),
+                tag=torch.zeros(0, 1, dtype=torch.int64, device=route_data.device),
+                load=load,
+            )
+            for _ in range(self.route_num)
+        ]
         for i in range(self.route_num):
-            indices = torch.nonzero(route_indices[:, i].view(-1)).to(inputs.device)
-            if indices.numel() > 0:
-                route_tags[i] = (
-                    torch.gather(tags, 0, indices) if tags is not None else indices
-                )
-                indices = indices.repeat(1, route_size).view(-1, *route_shape)
+            tag_indices = torch.nonzero(route_indices[:, i].view(-1)).to(
+                route_data.device
+            )
+            if tag_indices.numel() > 0:
+                results[i].tag = torch.gather(route_tag, 0, tag_indices)
+                data_indices = tag_indices.repeat(1, route_size).view(-1, *route_shape)
                 if self.transform:
                     gate = gates[:, i].reshape(
-                        (inputs.size(0),) + (1,) * len(route_shape)
+                        (route_data.size(0),) + (1,) * len(route_shape)
                     )
-                    route_results[i] = torch.gather(inputs * gate, 0, indices)
+                    results[i].data = torch.gather(route_data * gate, 0, data_indices)
                 else:
-                    route_results[i] = torch.gather(inputs, 0, indices)
-        return route_results, route_tags
+                    results[i].data = torch.gather(route_data, 0, data_indices)
+            if self.sparse:
+                results[i].load = tag_indices.numel()
+        return results
 
-    def combine(
-        self,
-        inputs: List[torch.Tensor],
-        tags: List[torch.Tensor],
-        loads: int = 0,
-    ) -> torch.Tensor:
+    def combine(self, inputs: List[FlowTensor]) -> FlowTensor:
         """
         Combine the outputs of the routers into the final outputs
         """
-        assert len(inputs) == self.route_num and len(tags) == self.route_num
-        route_shape = list(inputs[0].shape[1:])
-        inputs = torch.cat(inputs, dim=0)
-        tags = torch.cat(tags, dim=0)
-        if inputs.numel() == 0:
-            route_results = torch.zeros(
-                0, *route_shape, dtype=inputs.dtype, device=inputs.device
+        assert len(inputs) == self.route_num
+        route_datas = torch.cat([_input.data for _input in inputs], dim=0)
+        route_tags = torch.cat([_input.tag for _input in inputs], dim=0)
+        load = np.max([_input.load for _input in inputs])
+        route_shape = list(route_datas.shape[1:])
+        route_size = np.prod(route_shape)
+        if route_datas.numel() == 0:
+            results_data = torch.zeros(
+                0, *route_shape, dtype=route_datas.dtype, device=route_datas.device
             )
-            return route_results, tags
-        if loads == 0:
-            tags, inverse = torch.unique(tags, sorted=True, return_inverse=True)
-            route_indices = inverse.repeat(1, np.prod(route_shape)).view(
-                -1, *route_shape
+            return FlowTensor(results_data, route_tags, load)
+        if self.sparse:
+            result_tag, inverse = torch.unique(
+                route_tags, sorted=True, return_inverse=True
             )
-            # route_results = torch.scatter_reduce(inputs, 0, route_indices, self.reduction)
-            route_results = torch.zeros(
-                tags.size(0), *route_shape, dtype=inputs.dtype, device=inputs.device
-            ).scatter_(0, route_indices, inputs, reduce=self.reduction)
+            route_indices = inverse.repeat(1, route_size).view(-1, *route_shape)
+            load = result_tag.size(0)
         else:
-            route_indices = tags.repeat(1, np.prod(route_shape)).view(-1, *route_shape)
-            route_results = torch.zeros(
-                loads, *route_shape, dtype=inputs.dtype, device=inputs.device
-            ).scatter_(0, route_indices, inputs, reduce=self.reduction)
-        return route_results, tags
+            route_indices = route_tags.repeat(1, route_size).view(-1, *route_shape)
+            result_tag = torch.arange(
+                0, load, dtype=torch.int64, device=route_datas.device
+            )
+        result_data = torch.zeros(
+            load, *route_shape, dtype=route_datas.dtype, device=route_datas.device
+        ).scatter_(0, route_indices, route_datas, reduce=self.reduction)
+        # results_data = torch.scatter_reduce(route_datas, 0, route_indices, self.reduction)
+        return FlowTensor(result_data, route_tags, load)
---
+++ python/brt/router/flow_tensor.py
@@ -0,0 +1,34 @@
+import torch
+
+
+class FlowTensor(object):
+    CHECK_TAGS = False
+
+    def __init__(self, data: torch.Tensor, tag: torch.Tensor, load):
+        self.data = data
+        self.tag = tag
+        self.load = load
+
+    def __repr__(self):
+        return f"FlowTensor:\ndata: {self.data}\ntags: {self.tag}\nload: {self.load}"
+
+    @classmethod
+    def __torch_function__(cls, func, types, args=(), kwargs=None):
+        if kwargs is None:
+            kwargs = {}
+        new_args = []
+        new_tags = []
+        new_loads = []
+        for i, arg in enumerate(args):
+            new_args.append(arg.data if hasattr(arg, "data") else arg)
+            if hasattr(arg, "tag"):
+                new_tags.append(arg.tag)
+            if hasattr(arg, "load"):
+                new_loads.append(arg.load)
+        assert len(new_tags) > 0 and len(new_loads) > 0
+        ret = func(*new_args, **kwargs)
+        ret_flow_tensor = FlowTensor(ret, tag=new_tags[0], load=new_loads[0])
+        if cls.CHECK_TAGS:
+            for tag in new_tags:
+                assert torch.allclose(ret_flow_tensor.tag, tag)
+        return ret_flow_tensor
