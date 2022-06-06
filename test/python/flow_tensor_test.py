24983c06dd5e79e7978ec6df0cd9f7cc1b29c456
Raphael-Hao <raphaelhao@outlook.com>
2022-06-07 00:05:58 +0800

    [router] add template flow tensor test

---
+++ test/python/flow_tensor_test.py
@@ -0,0 +1,48 @@
+import unittest
+
+import torch
+from brt.router.flow_tensor import FlowTensor, deinit_flow_tensor, init_flow_tensor
+
+
+class FlowTensorTest(unittest.TestCase):
+    def test_flow_empty(self):
+        pass
+
+    def test_pack(self):
+        pass
+
+    def test_unpack(self):
+        pass
+
+    def test_deep_unpack(self):
+        pass
+
+    def test_deep_pack(self):
+        pass
+
+    def test_init_flow_tensor(self):
+        pass
+
+    def test_deinit_flow_tensor(self):
+        pass
+
+    def test_tag(self):
+        pass
+
+    def test_tag_stack(self):
+        pass
+
+    def test_load(self):
+        pass
+
+    def test_load_stack(self):
+        pass
+
+    def test_extra_attr(self):
+        pass
+
+    def test_extra_attr_stack(self):
+        pass
+    
+    def test_runtime_make_flow_tensor(self):
+        pass
