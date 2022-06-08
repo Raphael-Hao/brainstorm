f7092ef939381a0194ccb311761a9a78dc7001c2
Raphael-Hao <raphaelhao@outlook.com>
2022-06-08 21:35:17 +0800

    [router] random router

--- python/brt/router/scatter.py
+++ python/brt/router/scatter.py
@@ -19,8 +19,6 @@ from .symbolic import symbolic_scatter_route
 
 __all__ = [
     "ScatterRouter",
-    "RandomScatterRouter",
-    "FusedRandomScatterRouter",
 ]
 
 logger = log.get_logger(__file__)
@@ -183,82 +181,7 @@ class ScatterRouter(BaseRouter):
         return symbolic_scatter_route(inputs, self.dst_num)
 
 
-@router
-class RandomScatterRouter(ScatterRouter):
-    def __init__(
-        self,
-        dst_num: int,
-    ):
-        """random scatter router
-
-        Args:
-            dst_num (int): routing number
-        """
-
-        def route_func(inputs_data):
-            gates = torch.randn((inputs_data.size(0), dst_num))
-            return gates
-
-        super().__init__(
-            dst_num=dst_num,
-            route_func=route_func,
-            route_method="topk",
-            transform=False,
-            k=1,
-        )
 
 
-@router
-class MoEScatterRouter(ScatterRouter):
-    def __init__(
-        self,
-        global_expert,
-        topk=1,
-        post_score=False,
-    ):
-        super().__init__(
-            dst_num, route_func, route_method, residual_dst, transform, **kwargs
-        )
 
 
-@router
-class FusedRandomScatterRouter(ScatterRouter):
-    def __init__(
-        self,
-        dst_num: int,
-        supported_capacities: List[int],
-    ):
-        """random scatter router
-
-        Args:
-            dst_num (int): routing number
-        """
-        super().__init__(dst_num=dst_num)
-        self.supported_capacities = supported_capacities
-        # self.dispatcher = FusedDispatcher(self.dst_num, supported_capacities)
-
-    def route(
-        self, inputs: torch.Tensor
-    ) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]:
-        """routing logic of random router
-
-        Args:
-            inputs (_type_): inputs for routing
-
-        Returns:
-            Returns:
-            List[torch.Tensor]: routing results for each routing dst
-            List[torch.Tensor]: reverse indices for each routing dst
-            Union[int, torch.Size]]: indicate the reverse shape info
-        """
-
-        # calculate the scatter indices
-        route_dsts = torch.randint(
-            0, self.dst_num, (inputs.size(0),), device=inputs.device
-        )
-
-        # dispatch according to the indices
-        results, reverse_indices, capacities = self.dispatcher.dispatch(
-            inputs=inputs, route_dsts=route_dsts
-        )
-        return results, reverse_indices, capacities
