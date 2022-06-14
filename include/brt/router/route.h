db1de366397f9e83af30a0cf557ecc36b14c9a9c
Raphael-Hao <raphaelhao@outlook.com>
2022-06-14 12:13:24 +0800

    [cpp] add route back implementation

--- include/brt/router/route.h
+++ include/brt/router/route.h
@@ -9,18 +9,18 @@
 namespace brt {
 namespace router {
 
-void RouteDataWithLocalIndices(float* in_data /*[sample_num x sample_dim]*/,
-                               float* outdata /*[?load*dst_num x sample_dim]*/,
+void RouteWithLocalIndices(float* in_data /*[sample_num x sample_dim]*/,
+                           float* outdata /*[?load*dst_num x sample_dim]*/,
+                           float* gates /*[sample_num x dst_num]*/,
+                           int* route_indices /*[sample_num x dst_num]*/,
+                           int* dst_loads /*[dst_num]*/, int sample_num, int sample_dim,
+                           int dst_num, cudaStream_t stream);
+
+void RouteBackWithLocalIndices(float* in_data /*[?load*dst_num x sample_dim]*/,
+                               float* outdata /*[sample_num x sample_dim]*/,
                                float* gates /*[sample_num x dst_num]*/,
                                int* route_indices /*[sample_num x dst_num]*/,
                                int* dst_loads /*[dst_num]*/, int sample_num, int sample_dim,
                                int dst_num, cudaStream_t stream);
-
-void RouteDataWithGlobalIndices(float* in_data /*[sample_num x sample_dim]*/,
-                                float* outdata /*[?load*dst_num x sample_dim]*/,
-                                float* gates /*[sample_num x dst_num]*/,
-                                int* route_indices /*[sample_num x dst_num]*/,
-                                int* dst_loads /*[dst_num]*/, int sample_num, int dst_num,
-                                cudaStream_t stream);
 }  // namespace router
 }  // namespace brt
--- src/router/route.cu
+++ src/router/route.cu
@@ -56,17 +56,19 @@ __global__ void __launch_bounds__(1024) no_transform_route_back_with_local_indic
     int* __restrict__ route_indices /*[sample_num x dst_num]*/,
     int* __restrict__ dst_loads /*[dst_num]*/, int sample_num, int sample_dim, int dst_num) {
   for (int i = blockIdx.x; i < sample_num; i += gridDim.x) {
-    int route_index = i * dst_num + blockIdx.y;
-    int local_dst = route_indices[route_index];
-    if (local_dst == 0) {
-      continue;
-    }
-    int global_dst = local_dst - 1;
-    for (int j = 0; j < blockIdx.y; j++) {
-      global_dst += dst_loads[j];
-    }
-    for (int j = threadIdx.x; j < sample_dim; j += 1024) {
-      out_data[global_dst * sample_dim + j] = in_data[i * sample_dim + j];
+    for (int j = 0; j < dst_num; j++) {
+      int route_index = i * dst_num + j;
+      int local_dst = route_indices[route_index];
+      if (local_dst == 0) {
+        continue;
+      }
+      int global_dst = local_dst - 1;
+      for (int k = 0; k < j; k++) {
+        global_dst += dst_loads[k];
+      }
+      for (int k = threadIdx.x; k < sample_dim; k += 1024) {
+        out_data[i * sample_dim + k] += in_data[global_dst * sample_dim + k];
+      }
     }
   }
 }
@@ -79,29 +81,29 @@ __global__ void __launch_bounds__(1024)
                                   int* __restrict__ dst_loads /*[dst_num]*/, int sample_num,
                                   int sample_dim, int dst_num) {
   for (int i = blockIdx.x; i < sample_num; i += gridDim.x) {
-    int route_index = i * dst_num + blockIdx.y;
-
-    int local_dst = route_indices[route_index];
-    if (local_dst == 0) {
-      continue;
-    }
-    int global_dst = local_dst - 1;
-    for (int j = 0; j < blockIdx.y; j++) {
-      global_dst += dst_loads[j];
-    }
-
-    for (int j = threadIdx.x; j < sample_dim; j += 1024) {
-      out_data[global_dst * sample_dim + j] = in_data[i * sample_dim + j] * gates[route_index];
+    for (int j = 0; j < dst_num; j++) {
+      int route_index = i * dst_num + j;
+      int local_dst = route_indices[route_index];
+      if (local_dst == 0) {
+        continue;
+      }
+      int global_dst = local_dst - 1;
+      for (int k = 0; k < j; k++) {
+        global_dst += dst_loads[k];
+      }
+      for (int k = threadIdx.x; k < sample_dim; k += 1024) {
+        out_data[i * sample_dim + k] += in_data[global_dst * sample_dim + k] * gates[route_index];
+      }
     }
   }
 }
 
-void RouteDataWithLocalIndices(float* in_data /*[sample_num x sample_dim]*/,
-                               float* outdata /*[?load*dst_num x sample_dim]*/,
-                               float* gates /*[sample_num x dst_num]*/,
-                               int* route_indices /*[sample_num x dst_num]*/,
-                               int* dst_loads /*[dst_num]*/, int sample_num, int sample_dim,
-                               int dst_num, cudaStream_t stream) {
+void RouteWithLocalIndices(float* in_data /*[sample_num x sample_dim]*/,
+                           float* outdata /*[?load*dst_num x sample_dim]*/,
+                           float* gates /*[sample_num x dst_num]*/,
+                           int* route_indices /*[sample_num x dst_num]*/,
+                           int* dst_loads /*[dst_num]*/, int sample_num, int sample_dim,
+                           int dst_num, cudaStream_t stream) {
   constexpr dim3 block_size(1024);
   dim3 grid_size(512, dst_num);
   if (gates == nullptr) {
@@ -113,19 +115,19 @@ void RouteDataWithLocalIndices(float* in_data /*[sample_num x sample_dim]*/,
   }
 }
 
-void RouteDataBackWithLocalIndices(float* in_data /*[?load*dst_num x sample_dim]*/,
-                                   float* outdata /*[sample_num x sample_dim]*/,
-                                   float* gates /*[sample_num x dst_num]*/,
-                                   int* route_indices /*[sample_num x dst_num]*/,
-                                   int* dst_loads /*[dst_num]*/, int sample_num, int sample_dim,
-                                   int dst_num, cudaStream_t stream) {
+void RouteBackWithLocalIndices(float* in_data /*[?load*dst_num x sample_dim]*/,
+                               float* outdata /*[sample_num x sample_dim]*/,
+                               float* gates /*[sample_num x dst_num]*/,
+                               int* route_indices /*[sample_num x dst_num]*/,
+                               int* dst_loads /*[dst_num]*/, int sample_num, int sample_dim,
+                               int dst_num, cudaStream_t stream) {
   constexpr dim3 block_size(1024);
-  dim3 grid_size(512, dst_num);
+  dim3 grid_size(512);
   if (gates == nullptr) {
-    no_transform_route_with_local_indices<<<grid_size, block_size, 0, stream>>>(
+    no_transform_route_back_with_local_indices<<<grid_size, block_size, 0, stream>>>(
         in_data, outdata, route_indices, dst_loads, sample_num, sample_dim, dst_num);
   } else {
-    route_with_local_indices<<<grid_size, block_size, 0, stream>>>(
+    route_back_with_local_indices<<<grid_size, block_size, 0, stream>>>(
         in_data, outdata, gates, route_indices, dst_loads, sample_num, sample_dim, dst_num);
   }
 }
