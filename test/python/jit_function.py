a0db8fdbc8470a17db6321095ca4350d6becab20
Raphael-Hao <raphaelhao@outlook.com>
2022-06-20 00:11:31 +0800

    [misc] remove unused test files

--- test/python/jit_function.py
+++ test/python/jit_function.py
@@ -1,11 +1,11 @@
 import unittest
 
 import torch
-from brt.jit import (
-    CUDACompiler,
-    HeteroFusedModuleFunction,
-    HomoFusedModuleFunction,
-    HorizFusedModuleFunction,
+from brt.jit import CUDACompiler
+from brt.jit.function import (
+    HeteroFusedFunction,
+    HomoFusedFunction,
+    HorizFusedFunction,
     ModuleFunction,
 )
 
@@ -13,12 +13,14 @@ from brt.jit import (
 class JitFunctionTest(unittest.TestCase):
     def run_test(self, function, input):
         pass
-    
+
     def test_moduel_function(self):
-        ModuleFunction("Linear", input_infos={})
+        module_func = ModuleFunction("Linear", method="forward", input_infos={})
         pass
 
     def test_horiz_fused_function(self):
+        candidates = [ModuleFunction("Linear", method="forward") for _ in range(10)]
+        fused_function = HorizFusedFunction(candidates)
         pass
 
     def test_hetero_fused_function(self):
