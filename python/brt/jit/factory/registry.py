0c3ef5033b3ea7c4991784232cd8eda7476278f2
Raphael-Hao <raphaelhao@outlook.com>
2022-06-21 23:53:10 +0800

    [jit] FunctionFactory complete without check

--- python/brt/jit/factory/registry.py
+++ python/brt/jit/factory/registry.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2022 by Microsoft Corporation.
 # Licensed under the MIT license.
 
-from typing import Dict, List, Union
+from typing import Dict, List, Tuple, Union
 
 import torch
 
@@ -25,12 +25,20 @@ class ModuleInfo:
         raise NotImplementedError()
 
     @classmethod
-    def extract_argument_infos(
+    def extract_shared_arg_infos(
         cls,
         module: torch.nn.Module,
         method: str,
         sample_input: Union[torch.Tensor, List[torch.Tensor]],
-    ):
+    ) -> Tuple[List, List]:
+        raise NotImplementedError()
+
+    @classmethod
+    def extract_arg_infos(
+        cls,
+        module: torch.nn.Module,
+        method: str,
+    ) -> Tuple[int, int, List, List]:
         raise NotImplementedError()
 
     @classmethod
@@ -40,6 +48,9 @@ class ModuleInfo:
 
 class LinearInfo(ModuleInfo):
     __module_cls__ = torch.nn.Linear
+    __input_arg_indices__ = {"forward": [0]}
+    __parameter_arg_indices__ = {"forward": [1]}
+    __output_arg_indices__ = {"forward": [2]}
     __shared_arg_indices__ = {"forward": [0, 2]}
 
     @classmethod
@@ -69,7 +80,7 @@ class LinearInfo(ModuleInfo):
         ).load_from_db()
 
     @classmethod
-    def extract_argument_infos(
+    def extract_shared_arg_infos(
         cls, module: torch.nn.Linear, method: str, sample_input: torch.Tensor
     ):
         assert method in cls.__shared_arg_indices__, f"{method} is not supported"
@@ -80,14 +91,30 @@ class LinearInfo(ModuleInfo):
         return cls.__shared_arg_indices__[method], shared_arg_grans
 
     @classmethod
+    def extract_arg_infos(cls, module: torch.nn.Linear, method: str):
+        assert method in cls.__shared_arg_indices__, f"{method} is not supported"
+        if method.bias is None:
+            input_arg_num = 2
+        else:
+            input_arg_num = 3
+        total_arg_num = input_arg_num + 1
+        return (
+            input_arg_num,
+            total_arg_num,
+            cls.__input_arg_indices__[method],
+            cls.__output_arg_indices__[method],
+        )
+
+    @classmethod
     def get_output_init_func(cls, module: torch.nn.Linear, method: str):
         if method == "forward":
 
             def init_output(input):
+                # TODO we can move shape calculation out of the function
                 in_shape = input.shape
                 assert in_shape[-1] == module.in_features, "in_features is not matched"
                 out_shape = in_shape[:-1] + (module.out_features,)
-                return torch.zeros(out_shape, dtype=input.dtype, device=input.device)
+                return (torch.zeros(out_shape, dtype=input.dtype, device=input.device),)
 
         else:
             raise NotImplementedError(f"{method} is not supported")
@@ -96,6 +123,9 @@ class LinearInfo(ModuleInfo):
 
 class Conv2dBNActInfo(ModuleInfo):
     __module_cls__ = [torch.nn.Conv2d, torch.nn.BatchNorm2d, torch.nn.ReLU]
+    __input_arg_indices__ = {"forward": [0]}
+    __parameter_arg_indices__ = {"forward": [1]}
+    __output_arg_indices__ = {"forward": [2]}
     __shared_arg_indices__ = {"forward": [0, 2]}
 
     @classmethod
@@ -125,9 +155,7 @@ class Conv2dBNActInfo(ModuleInfo):
         assert method in cls.__shared_arg_indices__, f"{method} is not supported"
         if not isinstance(module, torch.nn.Sequential):
             module = torch.nn.Sequential(module)
-
-        module_name = "Conv2d"
-        module_name += "Bias" if module[0].bias is not None else ""
+        module_name = cls.get_module_name(module)
         parameters = {}
         parameters["in_channels"] = module[0].in_channels
         parameters["out_channels"] = module[0].out_channels
@@ -136,12 +164,6 @@ class Conv2dBNActInfo(ModuleInfo):
         parameters["padding"] = module[0].padding
         parameters["dilation"] = module[0].dilation
         parameters["groups"] = module[0].groups
-        if len(module) == 2:
-            module_name += (
-                "BatchNorm" if isinstance(module[1], torch.nn.BatchNorm2d) else "ReLU"
-            )
-        elif len(module) == 3:
-            module_name += "BatchNormReLU"
 
         sample_output = module(sample_input)
         input_infos = {"input_0": list(sample_input.shape)}
@@ -157,7 +179,7 @@ class Conv2dBNActInfo(ModuleInfo):
         ).load_from_db()
 
     @classmethod
-    def extract_argument_infos(
+    def extract_shared_arg_infos(
         cls, module: torch.nn.Module, method: str, sample_input: torch.Tensor
     ):
         assert method in cls.__shared_arg_indices__, f"{method} is not supported"
@@ -168,7 +190,27 @@ class Conv2dBNActInfo(ModuleInfo):
             sample_input_size * ModuleDTypeSizeInByte[sample_input.dtype],
             sample_output_size * ModuleDTypeSizeInByte[sample_output.dtype],
         ]
+
         return cls.__shared_arg_indices__[method], shared_arg_grans
+
+    @classmethod
+    def extract_arg_infos(cls, module: torch.nn.Module, method: str):
+        assert method in cls.__shared_arg_indices__, f"{method} is not supported"
+        if not isinstance(module, torch.nn.Sequential):
+            module = torch.nn.Sequential(module)
+        module_name = cls.get_module_name(module)
+        if module_name == "Conv2d" or module_name == "Conv2dReLU":
+            input_arg_num = 2
+        else:
+            input_arg_num = 3
+        total_arg_num = input_arg_num + 1
+
+        return (
+            input_arg_num,
+            total_arg_num,
+            cls.__input_arg_indices__[method],
+            cls.__output_arg_indices__[method],
+        )
 
     @classmethod
     def get_output_init_func(cls, module: torch.nn.Conv2d, method: str):
@@ -179,6 +221,7 @@ class Conv2dBNActInfo(ModuleInfo):
         if method == "forward":
 
             def init_output(input):
+                # TODO we can move shape calculation out of the function
                 in_shape = input.shape
                 if len(in_shape) == 3:
                     in_shape = (1,) + in_shape
@@ -204,9 +247,21 @@ class Conv2dBNActInfo(ModuleInfo):
                     if len(in_shape) == 4
                     else (out_c, out_h, out_w)
                 )
-                return torch.zeros(out_shape, dtype=input.dtype, device=input.device)
+                return (torch.zeros(out_shape, dtype=input.dtype, device=input.device),)
 
         else:
             raise NotImplementedError(f"{method} is not supported")
 
         return init_output
+
+    @classmethod
+    def get_module_name(cls, modules) -> str:
+        module_name = "Conv2d"
+        module_name += "Bias" if modules[0].bias is not None else ""
+        if len(modules) == 2:
+            module_name += (
+                "BatchNorm" if isinstance(modules[1], torch.nn.BatchNorm2d) else "ReLU"
+            )
+        elif len(modules) == 3:
+            module_name += "BatchNormReLU"
+        return module_name
