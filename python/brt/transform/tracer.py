7f30f1b4b02c2221de9cc2f858ca384e24be432b
Raphael-Hao <raphaelhao@outlook.com>
2022-07-08 00:54:19 +0800

    [brt] add new trace based on torch.fx

--- python/brt/transform/tracer.py
+++ python/brt/transform/tracer.py
@@ -7,6 +7,8 @@ import torch.fx as fx
 
 class BRTTRacer(fx.Tracer):
     def is_leaf_module(self, m: torch.nn.Module, module_qualified_name: str) -> bool:
-        if m.__module__.startswith("brt.routers"):
+        if m.__module__.startswith("brt.routers.fabrics") or m.__module__.startswith(
+            "brt.routers.protocols"
+        ):
             return True
         return super().is_leaf_module(m, module_qualified_name)
