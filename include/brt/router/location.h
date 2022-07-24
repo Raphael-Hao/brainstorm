7d0b975f2d5e0371dfaf3cb20ad01be9c01dc611
Raphael-Hao <raphaelhao@outlook.com>
2022-07-24 23:04:50 +0800

    [cpp] update header

--- include/brt/router/location.h
+++ include/brt/router/location.h
@@ -3,7 +3,8 @@
  * Licensed under the MIT license.
  */
 
-#pragma once
+#ifndef BRT_ROUTER_LOCATION_H_
+#define BRT_ROUTER_LOCATION_H_
 
 #include <brt/runtime/cuda_utils.h>
 
@@ -30,8 +31,8 @@ void GenerateSrcIndices(int* hot_mask /*[sample_num x path_num]*/,
                         const int& supported_capacity_num, cudaStream_t stream);
 
 void ConvertIndexFormat(int* origin_indices, int* new_indices, int* loads, const int& sample_num,
-                           const int& path_num, const int& target_index_fmt_id,
-                           cudaStream_t stream);
+                        const int& path_num, const int& target_index_fmt_id, cudaStream_t stream);
 
 }  // namespace router
 }  // namespace brt
+#endif  // BRT_ROUTER_LOCATION_H_
