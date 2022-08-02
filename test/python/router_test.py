6904e3352c91b4bada75caf819292bc493122ada
Raphael-Hao <raphaelhao@outlook.com>
2022-08-02 20:36:19 +0800

    update

--- test/python/router_test.py
+++ test/python/router_test.py
@@ -6,11 +6,8 @@ import unittest
 import torch
 import torch.nn as nn
 
-# from brt.runtime import log
 from brt.router import GatherRouter, ScatterRouter
 from brt.trace.graph import GraphTracer
-
-# log.set_level("router", "DEBUG")
 
 
 class BranchRoute(nn.Module):
@@ -52,37 +49,23 @@ class WeightedBranchRoute(BranchRoute):
 
     def forward(self, x):
         score = self.gate(x)
+        x_dim = x.dim()
         routed_results, routed_score = self.scatter_router(x, score)
         x_0 = self.expert1(routed_results[0])
         x_1 = self.expert2(routed_results[1])
 
-        weighted_x_0 = x_0 * routed_score[0]
-        weighted_x_1 = x_1 * routed_score[1]
+        weighted_x_0 = x_0 * (routed_score[0].view(-1, *([1] * (x_dim - 1))))
+        weighted_x_1 = x_1 * (routed_score[1].view(-1, *([1] * (x_dim - 1))))
         pre_x = self.gather_router([weighted_x_0, weighted_x_1])
 
         x = self.gather_router([x_0, x_1])
         score = self.gather_router([routed_score[0], routed_score[1]])
-        post_x = x * score
+        post_x = x * score.view(-1, *([1] * (x_dim - 1)))
 
         return pre_x, post_x
 
 
 class RouterTest(unittest.TestCase):
-    # def all_to_single(self, Model: BranchRoute, inputs, dst, sparse=False):
-    #     def gate(inputs):
-    #         gates = torch.zeros(
-    #             inputs.shape[0], 2, dtype=torch.float, device=inputs.device
-    #         )
-    #         gates[:, dst] = 1
-    #         return gates
-
-    #     model = Model(gate=gate, sparse=sparse)
-
-    #     results = model(inputs)
-    #     self.assertTrue(torch.allclose(results[dst].data, inputs))
-    #     self.assertTrue(results[1 - dst].data.numel() == 0)
-    #     self.assertTrue(torch.allclose(results[2].data, inputs))
-
     def simple_route(
         self,
         Model: BranchRoute,
@@ -153,28 +136,19 @@ class RouterTest(unittest.TestCase):
     ):
         def gate(inputs):
             gates = torch.zeros(
-                inputs.shape[0], 2, dtype=torch.int64, device=inputs.device
+                inputs.shape[0], 2, dtype=torch.float, device=inputs.device
             )
             if which_half == "upper":
                 gates[inputs.shape[0] // 2 :, dst] = 1
-            else:
+            elif which_half == "lower":
                 gates[: inputs.shape[0] // 2, dst] = 1
+            else:
+                gates[:, dst] = 1
             return gates
 
         model = Model(gate=gate, sparse=sparse)
         results = model(inputs)
-        if which_half == "upper":
-            self.assertTrue(
-                torch.allclose(results[0].data, inputs[inputs.size(0) // 2 :])
-            )
-            self.assertTrue(
-                torch.allclose(results[1].data, inputs[inputs.size(0) // 2 :])
-            )
-            self.assertTrue(torch.allclose(results[0].data, results[1].data))
-        else:
-            self.assertTrue(torch.allclose(results[0], inputs[: inputs.size(0) // 2]))
-            self.assertTrue(torch.allclose(results[1], inputs[: inputs.size(0) // 2]))
-            self.assertTrue(torch.allclose(results[0].data, results[1].data))
+        self.assertTrue(torch.allclose(results[0].data, results[1].data))
 
     def test_2d_route(self):
         for i in range(2):
@@ -185,6 +159,20 @@ class RouterTest(unittest.TestCase):
             self.simple_route(BranchRoute, x, dst=i, sparse=True)
             self.simple_route(BranchRoute, x, dst=i, which_half="upper", sparse=True)
             self.simple_route(BranchRoute, x, dst=i, which_half="lower", sparse=True)
+            self.weighted_route(WeightedBranchRoute, x, dst=i, sparse=False)
+            self.weighted_route(
+                WeightedBranchRoute, x, dst=i, which_half="upper", sparse=False
+            )
+            self.weighted_route(
+                WeightedBranchRoute, x, dst=i, which_half="lower", sparse=False
+            )
+            self.weighted_route(WeightedBranchRoute, x, dst=i, sparse=True)
+            self.weighted_route(
+                WeightedBranchRoute, x, dst=i, which_half="upper", sparse=True
+            )
+            self.weighted_route(
+                WeightedBranchRoute, x, dst=i, which_half="lower", sparse=True
+            )
 
     def test_3d_route(self):
         for i in range(2):
@@ -195,6 +183,20 @@ class RouterTest(unittest.TestCase):
             self.simple_route(BranchRoute, x, dst=i, sparse=True)
             self.simple_route(BranchRoute, x, dst=i, which_half="upper", sparse=True)
             self.simple_route(BranchRoute, x, dst=i, which_half="lower", sparse=True)
+            self.weighted_route(WeightedBranchRoute, x, dst=i, sparse=False)
+            self.weighted_route(
+                WeightedBranchRoute, x, dst=i, which_half="upper", sparse=False
+            )
+            self.weighted_route(
+                WeightedBranchRoute, x, dst=i, which_half="lower", sparse=False
+            )
+            self.weighted_route(WeightedBranchRoute, x, dst=i, sparse=True)
+            self.weighted_route(
+                WeightedBranchRoute, x, dst=i, which_half="upper", sparse=True
+            )
+            self.weighted_route(
+                WeightedBranchRoute, x, dst=i, which_half="lower", sparse=True
+            )
 
     def test_4d_route(self):
         for i in range(2):
@@ -205,6 +207,20 @@ class RouterTest(unittest.TestCase):
             self.simple_route(BranchRoute, x, dst=i, sparse=True)
             self.simple_route(BranchRoute, x, dst=i, which_half="upper", sparse=True)
             self.simple_route(BranchRoute, x, dst=i, which_half="lower", sparse=True)
+            self.weighted_route(WeightedBranchRoute, x, dst=i, sparse=False)
+            self.weighted_route(
+                WeightedBranchRoute, x, dst=i, which_half="upper", sparse=False
+            )
+            self.weighted_route(
+                WeightedBranchRoute, x, dst=i, which_half="lower", sparse=False
+            )
+            self.weighted_route(WeightedBranchRoute, x, dst=i, sparse=True)
+            self.weighted_route(
+                WeightedBranchRoute, x, dst=i, which_half="upper", sparse=True
+            )
+            self.weighted_route(
+                WeightedBranchRoute, x, dst=i, which_half="lower", sparse=True
+            )
 
     def test_trace(self):
         gate = nn.Sequential(nn.Linear(10, 2), nn.Softmax(dim=1))
@@ -213,14 +229,11 @@ class RouterTest(unittest.TestCase):
             model = Model(gate=gate)
             try:
                 graph_module = GraphTracer().trace(model)
-            #     script_simple_net = torch.jit.script(symbolize(model))
-            #     print(script_simple_net.graph)
-            #     print(script_simple_net.inlined_graph)
+                # TODO: add more tests
             except Exception as e:
                 self.fail(f"Failed to trace the graph through torch.fx: {e}")
 
         jit_script(BranchRoute)
-        jit_script(WeightedBranchRoute)
 
 
 if __name__ == "__main__":
