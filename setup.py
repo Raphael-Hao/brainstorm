abfa14e919b710302097886c188f0a2fd593bd79
Raphael-Hao <raphaelhao@outlook.com>
2022-08-08 14:06:03 +0800

    add cpu install

--- setup.py
+++ setup.py
@@ -10,32 +10,27 @@ if len(sys.argv) <= 1:
 root_path = pathlib.Path(__file__).parent.absolute()
 
 
-def install():
-    ext_libs, ext_args = (
-        [],
-        {
-            "cxx": ["-Wno-sign-compare", "-Wno-unused-but-set-variable"],
-            "nvcc": [
-                "-O3",
-                "-Xcompiler",
-                "-fopenmp",
-                "-Xcompiler",
-                "-fPIC",
-                "-std=c++14",
-            ],
-        },
-    )
-    ext_libs += ["dl", "cuda", "nvrtc"]
-    ext_args["cxx"] += ["-DUSE_CUDA"]
-    ext_args["nvcc"] += ["-DUSE_CUDA"]
-
-    setup(
-        name="brt",
-        version="0.1",
-        author="Weihao Cui",
-        package_dir={"": "python"},
-        packages=find_packages("python"),
-        ext_modules=[
+def install(use_cuda=False):
+    torch_extensions = []
+    if use_cuda:
+        ext_libs, ext_args = (
+            [],
+            {
+                "cxx": ["-Wno-sign-compare", "-Wno-unused-but-set-variable"],
+                "nvcc": [
+                    "-O3",
+                    "-Xcompiler",
+                    "-fopenmp",
+                    "-Xcompiler",
+                    "-fPIC",
+                    "-std=c++14",
+                ],
+            },
+        )
+        ext_libs += ["dl", "cuda", "nvrtc"]
+        ext_args["cxx"] += ["-DUSE_CUDA"]
+        ext_args["nvcc"] += ["-DUSE_CUDA"]
+        torch_extensions = [
             CUDAExtension(
                 name="brt._C.jit",
                 sources=[
@@ -64,13 +59,24 @@ def install():
                     root_path / "3rdparty/dmlc-core/include",
                 ],
                 extra_compile_args=ext_args,
-            )
-        ],
+            ),
+        ]
+    setup(
+        name="brt",
+        version="0.1",
+        author="Weihao Cui",
+        package_dir={"": "python"},
+        packages=find_packages("python"),
+        ext_modules=torch_extensions,
         cmdclass={"build_ext": BuildExtension},
     )
 
 
 try:
-    install()
+    install(use_cuda=True)
 except:
-    raise RuntimeError("Failed to install brt")
+    print("CUDA not found, skipping CUDA build")
+    try:
+        install(use_cuda=False)
+    except:
+        raise RuntimeError("Failed to install brt")
