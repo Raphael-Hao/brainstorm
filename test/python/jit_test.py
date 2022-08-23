d2f76e9c1164b3d61aa20b3934fd31457d69ee8f
Raphael-Hao <raphaelhao@outlook.com>
2022-08-23 21:02:11 +0800

    add unittest for hetero fusion

--- test/python/jit_test.py
+++ test/python/jit_test.py
@@ -54,14 +54,66 @@ class JitFunctionTest(unittest.TestCase):
             )
 
     def test_hetero_fused_function(self):
-        linears = nn.ModuleList(nn.Linear(512, 1024) for i in range(4))
-        sample_inputs = [torch.randn((16, 512)) for i in range(4)]
+        linears = nn.ModuleList(nn.Linear(512, 1024) for _ in range(4)).cuda()
+        sample_inputs = [torch.randn((16, 512)).cuda() for _ in range(4)]
         hetero_fused_kernel = make_jit_kernel(
             linears, sample_inputs=sample_inputs, opt_level="hetero_fuse"
         )
+        active_blocks = [0, 1, 0, 1]
+
+        hetero_fused_inputs = []
+
+        for i, active in enumerate(active_blocks):
+            if active == 0:
+                hetero_fused_inputs.append(torch.randn((0, 512)).cuda())
+                hetero_fused_inputs.append(linears[i].weight)
+                hetero_fused_inputs.append(torch.empty((0, 1024)).cuda())
+                hetero_fused_inputs.append(linears[i].bias)
+            else:
+                hetero_fused_inputs.append(sample_inputs[i])
+                hetero_fused_inputs.append(linears[i].weight)
+                hetero_fused_inputs.append(torch.empty((16, 1024)).cuda())
+                hetero_fused_inputs.append(linears[i].bias)
+        hetero_fused_kernel(*hetero_fused_inputs, active_blocks=active_blocks)
+        for i, active in enumerate(active_blocks):
+            if active != 0:
+                sample_out = linears[i](hetero_fused_inputs[i * 4])
+                self.assertTrue(
+                    torch.allclose(
+                        sample_out, hetero_fused_inputs[2 + i * 4], atol=1e-6
+                    )
+                )
 
     def test_homo_fused_function(self):
-        pass
+        linears = nn.ModuleList(nn.Linear(512, 1024) for _ in range(4)).cuda()
+        sample_inputs = [torch.randn((16, 512)).cuda() for _ in range(4)]
+        hetero_fused_kernel = make_jit_kernel(
+            linears, sample_inputs=sample_inputs, opt_level="homo_fuse"
+        )
+        active_blocks = [0, 1, 0, 1]
+
+        hetero_fused_inputs = []
+
+        for i, active in enumerate(active_blocks):
+            if active == 0:
+                hetero_fused_inputs.append(torch.randn((0, 512)).cuda())
+                hetero_fused_inputs.append(linears[i].weight)
+                hetero_fused_inputs.append(torch.empty((0, 1024)).cuda())
+                hetero_fused_inputs.append(linears[i].bias)
+            else:
+                hetero_fused_inputs.append(sample_inputs[i])
+                hetero_fused_inputs.append(linears[i].weight)
+                hetero_fused_inputs.append(torch.empty((16, 1024)).cuda())
+                hetero_fused_inputs.append(linears[i].bias)
+        hetero_fused_kernel(*hetero_fused_inputs, active_blocks=active_blocks)
+        for i, active in enumerate(active_blocks):
+            if active != 0:
+                sample_out = linears[i](hetero_fused_inputs[i * 4])
+                self.assertTrue(
+                    torch.allclose(
+                        sample_out, hetero_fused_inputs[2 + i * 4], atol=1e-6
+                    )
+                )
 
 
 if __name__ == "__main__":
