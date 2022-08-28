751a3c515a22d10512af434d430b33595a8516a6
Raphael-Hao <raphaelhao@outlook.com>
2022-08-28 23:50:32 +0800

    Attach the pinned memory to the parameters and buffers to avoid extra pin memory allocation

--- python/brt/runtime/weight_load.py
+++ python/brt/runtime/weight_load.py
@@ -1,58 +1,102 @@
 # Copyright (c) 2022 by Microsoft Corporation.
 # Licensed under the MIT license.
 import torch
+import torch.nn as nn
+from torch.nn.parameter import Parameter
+
 
 class WeightLoader:
-    _stream: torch.cuda.Stream
+    _stream: torch.cuda.Stream = None
 
-    def _apply(self, fn):
-        for module in self.children():
-            module._apply(fn)
+    @classmethod
+    def init(cls, stream=None):
+        if stream is None:
+            cls._stream = torch.cuda.default_stream()
+        else:
+            cls._stream = stream
 
-        def compute_should_use_set_data(tensor, tensor_applied):
-            if torch._has_compatible_shallow_copy_type(tensor, tensor_applied):
-                # If the new tensor has compatible tensor type as the existing tensor,
-                # the current behavior is to change the tensor in-place using `.data =`,
-                # and the future behavior is to overwrite the existing tensor. However,
-                # changing the current behavior is a BC-breaking change, and we want it
-                # to happen in future releases. So for now we introduce the
-                # `torch.__future__.get_overwrite_module_params_on_conversion()`
-                # global flag to let the user control whether they want the future
-                # behavior of overwriting the existing tensor or not.
-                return not torch.__future__.get_overwrite_module_params_on_conversion()
-            else:
-                return False
+    @classmethod
+    def pin_memory(cls, m: nn.Module):
+        for module in m.children():
+            cls.pin_memory(module)
 
-        for key, param in self._parameters.items():
+        for key, param in m._parameters.items():
+            if param is None:
+                continue
+            with torch.no_grad():
+                param.data = param.pin_memory()
+            if param.grad is not None:
+                with torch.no_grad():
+                    param.grad.data = param.grad.pin_memory()
+
+        for key, buf in m._buffers.items():
+            if buf is not None:
+                m._buffers[key] = buf.pin_memory()
+
+    @classmethod
+    def load(cls, m: nn.Module):
+        for module in m.children():
+            cls.load(module)
+
+        for key, param in m._parameters.items():
             if param is None:
                 continue
             # Tensors stored in modules are graph leaves, and we don't want to
             # track autograd history of `param_applied`, so we have to use
             # `with torch.no_grad():`
             with torch.no_grad():
-                param_applied = fn(param)
-            should_use_set_data = compute_should_use_set_data(param, param_applied)
-            if should_use_set_data:
-                param.data = param_applied
-                out_param = param
-            else:
-                assert isinstance(param, Parameter)
-                assert param.is_leaf
-                out_param = Parameter(param_applied, param.requires_grad)
-                self._parameters[key] = out_param
+                with torch.cuda.stream(cls._stream):
+                    param_applied = param.cuda(non_blocking=True)
+            param.pin_cpu_data = param.data
+            param.data = param_applied
+            out_param = param
 
             if param.grad is not None:
                 with torch.no_grad():
-                    grad_applied = fn(param.grad)
-                should_use_set_data = compute_should_use_set_data(param.grad, grad_applied)
-                if should_use_set_data:
-                    out_param.grad.data = grad_applied
-                else:
-                    assert param.grad.is_leaf
-                    out_param.grad = grad_applied.requires_grad_(param.grad.requires_grad)
+                    with torch.cuda.stream(cls._stream):
+                        grad_applied = param.grad.cuda(non_blocking=True)
 
-        for key, buf in self._buffers.items():
+                out_param.grad.pin_cpu_data = param.grad.data
+                out_param.grad.data = grad_applied
+
+        for key, buf in m._buffers.items():
             if buf is not None:
-                self._buffers[key] = fn(buf)
+                with torch.cuda.stream(cls._stream):
+                    m._buffers[key] = buf.cuda(non_blocking=True)
+                m._buffers[key].pin_cpu_data = buf
 
-        return self
+        return m
+
+    @classmethod
+    def unload(cls, m: nn.Module):
+        for module in m.children():
+            cls.unload(module)
+
+        for key, param in m._parameters.items():
+            if param is None:
+                continue
+            # Tensors stored in modules are graph leaves, and we don't want to
+            # track autograd history of `param_applied`, so we have to use
+            # `with torch.no_grad():`
+            with torch.no_grad():
+                with torch.cuda.stream(cls._stream):
+                    param_applied = param.pin_cpu_data
+                    param_applied.copy_(param.data, non_blocking=True)
+            param.data = param_applied
+            out_param = param
+
+            if param.grad is not None:
+                with torch.no_grad():
+                    with torch.cuda.stream(cls._stream):
+                        grad_applied = param.grad.pin_cpu_data
+                        grad_applied.copy_(param.grad.data, non_blocking=True)
+                out_param.grad.data = grad_applied
+
+        for key, buf in m._buffers.items():
+            if buf is not None:
+                with torch.cuda.stream(cls._stream):
+                    buf_applied = buf.pin_cpu_data
+                    buf_applied.copy_(buf, non_blocking=True)
+                m._buffers[key] = buf_applied
+
+        return m
---
+++ sample/misc/weight_load.py
@@ -0,0 +1,52 @@
+# Copyright (c) 2022 by Microsoft Corporation.
+# Licensed under the MIT license.
+
+import torch
+import time
+from torch.utils import data
+
+start_event = torch.cuda.Event(enable_timing=True)
+end_event = torch.cuda.Event(enable_timing=True)
+
+
+a = torch.randn(64, 3, 1024, 1024, device="cuda")
+torch.cuda.synchronize()
+
+start_stamp = time.time()
+b = a.to("cpu", non_blocking=True)
+end_stamp = time.time()
+print(f"cpu time non blocking to('cpu'): {(end_stamp - start_stamp) * 1000:.2f} ms")
+
+torch.cuda.synchronize()
+# start_stamp = time.time()
+b = torch.empty_like(a, device="cpu", pin_memory=True)
+# b = torch.empty_like(a, device="cpu")
+start_stamp = time.time()
+b.copy_(a, non_blocking=True)
+end_stamp = time.time()
+print(f"cpu time copy_-based non blocking to('cpu'): {(end_stamp - start_stamp) * 1000:.2f} ms")
+
+
+torch.cuda.synchronize()
+start_event.record()
+b = a.to("cpu", non_blocking=True)
+end_event.record()
+end_event.synchronize()
+print(f"gpu time non blocking to('cpu'): {start_event.elapsed_time(end_event):.2f} ms")
+
+
+a = torch.randn(64, 3, 1024, 1024, device="cpu", pin_memory=True)
+torch.cuda.synchronize()
+
+start_stamp = time.time()
+b = a.to("cuda", non_blocking=True)
+end_stamp = time.time()
+print(f"cpu time non blocking to('cuda'): {(end_stamp - start_stamp) * 1000:.2f} ms")
+
+
+torch.cuda.synchronize()
+start_event.record()
+b = a.to("cuda", non_blocking=True)
+end_event.record()
+end_event.synchronize()
+print(f"gpu time non blocking to('cuda'): {start_event.elapsed_time(end_event):.2f} ms")
