bf894d27f1bd48b575aa42191296967c3b74777e
Raphael-Hao <raphaelhao@outlook.com>
2022-08-31 15:58:51 +0800

    renaming bound_check to liveness

--- python/brt/passes/horiz_fuse.py
+++ python/brt/passes/horiz_fuse.py
@@ -11,10 +11,5 @@ class HorizFusePass(PassBase):
     jit_need_modules = [nn.Conv2d, nn.Linear]
     native_modules = [nn.ReLU, nn.Tanh, nn.Sigmoid, nn.Softmax]
 
-    @classmethod
-    def run_on_graph(cls, graph):
-        for module in graph.modules():
-            if isinstance(module, cls.jit_need_modules):
-                module.fuse_parameters()
-            if isinstance(module, cls.native_modules):
-                module.fuse_parameters()
+    def run_on_graph(self):
+        pass
