7c18d7b5efab498e9d9f78c2ffb1099899673647
Raphael-Hao <raphaelhao@outlook.com>
2022-08-31 15:58:51 +0800

    renaming bound_check to liveness

--- python/brt/passes/__init__.py
+++ python/brt/passes/__init__.py
@@ -1,4 +1,4 @@
 # Copyright (c) 2022 by Microsoft Corporation.
 # Licensed under the MIT license.
-from brt.passes import bound_check
+from brt.passes import liveness
 from brt.passes.base import register_pass, get_pass
