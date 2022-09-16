20b06d207cf2fb62a4fa8e167b7a0d26e3c41bf4
Lingji Ouyang <ouyanglingji@gmail.com>
2022-09-16 08:23:17 +0000

    [benchmark-classsr] update db

--- scripts/kernel_db_update.py
+++ scripts/kernel_db_update.py
@@ -1,13 +1,13 @@
-from python.brt.jit.codegen.storage import KernelStorager
+from brt.jit.codegen.storage import KernelStorager
 import json
-from typing import Dict
+from typing import Dict, List
 from collections import OrderedDict
 
 kernel_storager = KernelStorager()
 cursor = kernel_storager.cursor
 
 SELECT_ALL_CMD = r"""
-SELECT Key, Identifier
+SELECT Key, Identifier, OpType 
 FROM   KernelCache"""
 SELECT_KEY_CMD = r"""
 SELECT Identifier
@@ -19,17 +19,10 @@ SET Identifier = ?
 WHERE Key = ?
 """
 qrs = cursor.execute(SELECT_ALL_CMD).fetchall()
-for key, id in qrs:
+for key, id, optype in qrs:
     id: Dict = json.loads(id)
-    id['input_infos'] = OrderedDict(sorted(id['input_infos'].items()))
-    id['output_infos'] = OrderedDict(sorted(id['output_infos'].items()))
-    id['parameters'] = OrderedDict(sorted(id['parameters'].items()))
-    id = OrderedDict(sorted(id.items()))
-    id = json.dumps(id)
-    print(id)
-    cursor.execute(UPDATE_CMD, (id, key))
-    cursor.connection.commit()
 
+    # # Delete parameters.module_name
     # popret = id['parameters'].pop('module_name', None)
     # if popret is not None:
     #     print(id['parameters'])
@@ -37,3 +30,33 @@ for key, id in qrs:
     #     cursor.execute(UPDATE_CMD, (id, key))
     #     cursor.connection.commit()
         
+    # Update output size of ConvTranspose2d
+    # {
+    #     "output_infos": {
+    #         "output_0": [34, 3, 32, 32]
+    #     },
+    #     ...
+    # }
+    if optype == 'ConvTranspose2dBias':
+        outinfo: List = id['output_infos']['output_0']
+        if outinfo is None:
+            id['output_infos']['output_0'] = [id['input_infos']['input_0'][0], 3, 128, 128]
+        else:
+            outinfo[:2].extend([128, 128])
+            id['output_infos']['output_0'] = outinfo
+        print(f"{id['output_infos']['output_0']}")
+
+    
+    # Sort id
+    id['input_infos'] = OrderedDict(sorted(id['input_infos'].items()))
+    id['output_infos'] = OrderedDict(sorted(id['output_infos'].items()))
+    id['parameters'] = OrderedDict(sorted(id['parameters'].items()))
+    id = OrderedDict(sorted(id.items()))
+
+
+    id = json.dumps(id)
+    # print(id)
+    cursor.execute(UPDATE_CMD, (id, key))
+    cursor.connection.commit()
+        
+
