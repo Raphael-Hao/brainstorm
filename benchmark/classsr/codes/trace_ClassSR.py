3dec33a85112a432b32ec5b1056a7feb7aa658a0
Lingji Ouyang <ouyanglingji@gmail.com>
2022-09-20 16:10:01 +0800

    [benchmark-classsr] fused classsr using Conv2d+PReLU

--- benchmark/classsr/codes/trace_ClassSR.py
+++ benchmark/classsr/codes/trace_ClassSR.py
@@ -5,6 +5,7 @@ from models.archs.classSR_fsrcnn_arch import classSR_3class_fsrcnn_net
 from models.archs.classSR_carn_arch import ClassSR as classSR_3class_carn_net
 from models.archs.classSR_srresnet_arch import ClassSR as classSR_3class_srresnet_net
 from models.archs.classSR_rcan_arch import classSR_3class_rcan as classSR_3class_rcan_net
+from models.archs.classSR_fused_fsrcnn_arch import classSR_3class_fused_fsrcnn_net
 
 import torch
 import torch.nn as nn
@@ -22,9 +23,13 @@ if __name__ == "__main__":
         'classsr_carn': classSR_3class_carn_net,
         'classsr_srresnet': classSR_3class_srresnet_net,
         'classsr_rcan': classSR_3class_rcan_net,
+        'fused_classsr_fsrcnn': classSR_3class_fused_fsrcnn_net,
     }
     for model_name in models:
-        model = models[model_name]()
+        if 'fused' in model_name:
+            model = models[model_name](models[model_name[5:]]())
+        else:
+            model = models[model_name]()
         # model.to('cuda:0')
         model.eval()
         # x = torch.arange(0, 4 * 3 * 32 * 32, dtype=torch.long).view(4, 3, 32, 32)
@@ -38,3 +43,4 @@ if __name__ == "__main__":
         graph_drawer = FxGraphDrawer(graph_module, "brt_model")
         with open(BRT_CACHE_PATH/f"transformed_model/classsr/{model_name}_graph.svg", "wb") as f:
             f.write(graph_drawer.get_dot_graph().create_svg())
+        print(f'{model_name} done.')
