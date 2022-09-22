ba72206701f16be8dbb9ff916440508f9aef627f
Raphael-Hao <raphaelhao@outlook.com>
2022-09-22 18:27:10 +0800

    update to multiprocess version

--- benchmark/swin_moe/data/imagnet22k_extract_valid.py
+++ benchmark/swin_moe/data/imagnet22k_extract_valid.py
@@ -1,10 +1,11 @@
 # Copyright (c) 2022 by Microsoft Corporation.
 # Licensed under the MIT license.
-
+from typing import List
 import json
 from PIL import Image
 import pathlib
 import argparse
+import multiprocessing as mp
 
 
 def _load_image(path):
@@ -19,20 +20,44 @@ def _save_image(im: Image, base_path: pathlib.Path, name: str):
     return im
 
 
-def extract_valid(ann_path, origin_path, new_path):
-    ann_path = pathlib.Path(ann_path)
-    origin_path = pathlib.Path(origin_path)
-    new_path = pathlib.Path(new_path)
-    database = json.load(open(ann_path))
-    for i in range(len(database)):
-        idb = database[i]
-        images = _load_image(origin_path / idb[0])
-        _save_image(images, new_path, idb[0])
+class Extractor(mp.Process):
+    def __init__(self, ann_path, origin_path, new_path, start_id, end_id):
+        super().__init__()
+        self.ann_path = pathlib.Path(ann_path)
+        self.origin_path = pathlib.Path(origin_path)
+        self.new_path = pathlib.Path(new_path)
+        self.start_id = start_id
+        self.end_id = end_id
 
-if __name__ == '__main__':
+    def run(self):
+        self.database = json.load(open(self.ann_path))
+        for i in range(self.start_id, self.end_id):
+            idb = database[i]
+            images = _load_image(self.origin_path / idb[0])
+            _save_image(images, self.new_path, idb[0])
+
+
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--ann_path', type=str, required=True)
-    parser.add_argument('--origin_path', type=str, required=True)
-    parser.add_argument('--new_path', type=str, required=True)
+    parser.add_argument("--ann_path", type=str, required=True)
+    parser.add_argument("--origin_path", type=str, required=True)
+    parser.add_argument("--new_path", type=str, required=True)
+    parser.add_argument("--num_workers", type=int, default=8)
     args = parser.parse_args()
-    extract_valid(args.ann_path, args.origin_path, args.new_path)
+    database = json.load(open(args.ann_path))
+    all_images = len(database)
+    print(f"Total images: {all_images}")
+    num_workers = args.num_workers
+    start_id = 0
+    image_per_worker = int(all_images // num_workers)
+    extractors: List[mp.Process] = []
+    for i in range(num_workers):
+        end_id = start_id + image_per_worker if i < num_workers - 1 else all_images
+        extractors.append(
+            Extractor(args.ann_path, args.origin_path, args.new_path, start_id, end_id)
+        )
+        extractors[-1].start()
+        start_id += image_per_worker
+    for extractor in extractors:
+        extractor.join()
+
