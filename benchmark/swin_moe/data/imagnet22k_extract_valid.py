9216617aec77391ec533f47d544eb78b6e7c36c5
Raphael-Hao <raphaelhao@outlook.com>
2022-09-22 17:15:25 +0800

    add script to extract valid database

---
+++ benchmark/swin_moe/data/imagnet22k_extract_valid.py
@@ -0,0 +1,36 @@
+# Copyright (c) 2022 by Microsoft Corporation.
+# Licensed under the MIT license.
+
+import json
+from PIL import Image
+import pathlib
+import argparse
+
+
+def _load_image(path):
+    im = Image.open(path)
+    return im
+
+
+def _save_image(im: Image, base_path: pathlib.Path, name: str):
+    im.save(base_path / name)
+    return im
+
+
+def extract_valid(ann_path, origin_path, new_path):
+    ann_path = pathlib.Path(ann_path)
+    origin_path = pathlib.Path(origin_path)
+    new_path = pathlib.Path(new_path)
+    database = json.load(open(ann_path))
+    for i in range(len(database)):
+        idb = database[i]
+        images = _load_image(origin_path / idb[0])
+        _save_image(images, new_path, idb[0])
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--ann_path', type=str, required=True)
+    parser.add_argument('--origin_path', type=str, required=True)
+    parser.add_argument('--new_path', type=str, required=True)
+    args = parser.parse_args()
+    extract_valid(args.ann_path, args.origin_path, args.new_path)
