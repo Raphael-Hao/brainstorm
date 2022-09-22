a82d607609eb3eee2b247b8021ac9e608f268164
Lingji Ouyang <ouyanglingji@gmail.com>
2022-09-22 04:47:41 +0000

    [benchmark-classsr] update result

--- benchmark/classsr/codes/models/archs/Fused_FSRCNN_arch.py
+++ benchmark/classsr/codes/models/archs/Fused_FSRCNN_arch.py
@@ -17,147 +17,145 @@ class Fused_FSRCNN_net(torch.nn.Module):
         self.bs = bs
         self.d = raw.d # 16
         self.s = raw.s # 12
-        self.head_conv = make_jit_kernel(raw.head_conv, torch.randn(bs, 3, 32, 32).cuda())
+        for name, tensor in raw.named_parameters():
+            if 'tail' not in name:
+                self.register_parameter(name.replace(".", "_"), tensor)
+        for name, tensor in raw.named_buffers():
+            if 'tail' not in name:
+                self.register_buffer(name.replace(".", "_"), tensor)
+        self.head_conv = make_jit_kernel(raw.head_conv, torch.randn(bs, 3, 32, 32, device='cuda'))
         self.head_conv_args = [ # Conv2d+PReLU
             None,
-            raw.head_conv[0].weight,
+            self.head_conv_0_weight,
             None,
-            raw.head_conv[0].bias,
-            raw.head_conv[1].weight,
+            self.head_conv_0_bias,
+            self.head_conv_1_weight,
         ]
         self.body_conv = [
             make_jit_kernel(
                 raw.body_conv[0],
-                torch.randn(bs, self.d, 32, 32)
+                torch.randn(bs, self.d, 32, 32, device='cuda')
             ),
             make_jit_kernel(
                 raw.body_conv[1],
-                torch.randn(bs, self.s, 32, 32)
+                torch.randn(bs, self.s, 32, 32, device='cuda')
             ),
             make_jit_kernel(
                 raw.body_conv[2],
-                torch.randn(bs, self.s, 32, 32)
+                torch.randn(bs, self.s, 32, 32, device='cuda')
             ),
             make_jit_kernel(
                 raw.body_conv[3],
-                torch.randn(bs, self.s, 32, 32)
+                torch.randn(bs, self.s, 32, 32, device='cuda')
             ),
             make_jit_kernel(
                 nn.Sequential(
                     raw.body_conv[4],
                     raw.body_conv[5],
                 ),
-                torch.randn(bs, self.s, 32, 32)
+                torch.randn(bs, self.s, 32, 32, device='cuda')
             ),
             make_jit_kernel(
                 raw.body_conv[6],
-                torch.randn(bs, self.d, 32, 32)
+                torch.randn(bs, self.s, 32, 32, device='cuda')
             ),
         ]
         self.body_conv_args = [
             [ # 0 Conv2d+PReLU 
                 None,
-                raw.body_conv[0].weight,
+                self.body_conv_0_0_weight,
                 None,
-                raw.body_conv[0].bias,
-                raw.body_conv[1].weight,
+                self.body_conv_0_0_bias,
+                self.body_conv_0_1_weight,
             ],
             [ # 1 Conv2d 
                 None,
-                raw.body_conv[1].weight,
+                self.body_conv_1_weight,
                 None,
-                raw.body_conv[1].bias,
+                self.body_conv_1_bias,
             ],
             [ # 2 Conv2d 
                 None,
-                raw.body_conv[2].weight,
+                self.body_conv_2_weight,
                 None,
-                raw.body_conv[2].bias,
+                self.body_conv_2_bias,
             ],
             [ # 3 Conv2d 
                 None,
-                raw.body_conv[3].weight,
+                self.body_conv_3_weight,
                 None,
-                raw.body_conv[3].bias,
+                self.body_conv_3_bias,
             ],
             [ # 4 Conv2d+PReLU 
                 None,
-                raw.body_conv[4].weight,
+                self.body_conv_4_weight,
                 None,
-                raw.body_conv[4].bias,
-                raw.body_conv[5].weight,
+                self.body_conv_4_bias,
+                self.body_conv_5_weight,
             ],
             [ # 5 Conv2d+PReLU 
                 None,
-                raw.body_conv[6].weight,
+                self.body_conv_6_0_weight,
                 None,
-                raw.body_conv[6].bias,
-                raw.body_conv[6].weight,
+                self.body_conv_6_0_bias,
+                self.body_conv_6_1_weight,
             ],
         ]
-        self.tail_conv = make_jit_kernel(raw.tail_conv, torch.randn(bs, self.d, 32, 32))
-        self.tail_conv_args =  [ # ConvTranspose2d
-            raw.tail_conv.weight,
-            None,
-            raw.tail_conv.bias,
-            None,
-        ]
+        self.tail_conv = raw.tail_conv
+
+
     def forward(self, x):
+
         self.head_conv_args[0] = x
-        self.head_conv_args[2] = torch.empty(self.bs, self.d, 32 ,32).cuda()
+        self.head_conv_args[2] = torch.empty(self.bs, self.d, 32, 32, device='cuda')
         self.head_conv(*self.head_conv_args)
         x = self.head_conv_args[2]
         self.head_conv_args[0] = None
         self.head_conv_args[2] = None
         
         self.body_conv_args[0][0] = x
-        self.body_conv_args[0][2] = torch.empty(self.bs, self.s, 32 ,32).cuda()
-        self.body_conv[0](*self.head_conv_args[0])
+        self.body_conv_args[0][2] = torch.empty(self.bs, self.s, 32, 32, device='cuda')
+        self.body_conv[0](*self.body_conv_args[0])
         x = self.body_conv_args[0][2]
         self.body_conv_args[0][0] = None
         self.body_conv_args[0][2] = None
         
         self.body_conv_args[1][0] = x
-        self.body_conv_args[1][2] = torch.empty(self.bs, self.s, 32 ,32).cuda()
-        self.body_conv[1](*self.head_conv_args[1])
-        x = self.body_conv_args[0][2]
+        self.body_conv_args[1][2] = torch.empty(self.bs, self.s, 32, 32, device='cuda')
+        self.body_conv[1](*self.body_conv_args[1])
+        x = self.body_conv_args[1][2]
         self.body_conv_args[1][0] = None
         self.body_conv_args[1][2] = None
         
         self.body_conv_args[2][0] = x
-        self.body_conv_args[2][2] = torch.empty(self.bs, self.s, 32 ,32).cuda()
-        self.body_conv[2](*self.head_conv_args[2])
-        x = self.body_conv_args[0][2]
+        self.body_conv_args[2][2] = torch.empty(self.bs, self.s, 32, 32, device='cuda')
+        self.body_conv[2](*self.body_conv_args[2])
+        x = self.body_conv_args[2][2]
         self.body_conv_args[2][0] = None
         self.body_conv_args[2][2] = None
         
         self.body_conv_args[3][0] = x
-        self.body_conv_args[3][2] = torch.empty(self.bs, self.s, 32 ,32).cuda()
-        self.body_conv[3](*self.head_conv_args[3])
-        x = self.body_conv_args[0][2]
+        self.body_conv_args[3][2] = torch.empty(self.bs, self.s, 32, 32, device='cuda')
+        self.body_conv[3](*self.body_conv_args[3])
+        x = self.body_conv_args[3][2]
         self.body_conv_args[3][0] = None
         self.body_conv_args[3][2] = None
         
         self.body_conv_args[4][0] = x
-        self.body_conv_args[4][2] = torch.empty(self.bs, self.s, 32 ,32).cuda()
-        self.body_conv[4](*self.head_conv_args[4])
-        x = self.body_conv_args[0][2]
+        self.body_conv_args[4][2] = torch.empty(self.bs, self.s, 32, 32, device='cuda')
+        self.body_conv[4](*self.body_conv_args[4])
+        x = self.body_conv_args[4][2]
         self.body_conv_args[4][0] = None
         self.body_conv_args[4][2] = None
         
         self.body_conv_args[5][0] = x
-        self.body_conv_args[5][2] = torch.empty(self.bs, self.s, 32 ,32).cuda()
-        self.body_conv[5](*self.head_conv_args[5])
-        x = self.body_conv_args[0][2]
+        self.body_conv_args[5][2] = torch.empty(self.bs, self.d, 32, 32, device='cuda')
+        self.body_conv[5](*self.body_conv_args[5])
+        x = self.body_conv_args[5][2]
         self.body_conv_args[5][0] = None
         self.body_conv_args[5][2] = None
 
-        
-        self.tail_conv_args[0] = x
-        self.tail_conv_args[2] = torch.empty(self.bs, 3, 32 ,32).cuda()
-        self.tail_conv(*self.tail_conv_args)
-        x = self.tail_conv_args[2]
-        self.tail_conv_args[0] = None
+        x = self.tail_conv(x)
 
         return x
         
--- benchmark/classsr/codes/models/archs/classSR_fused_fsrcnn_arch.py
+++ benchmark/classsr/codes/models/archs/classSR_fused_fsrcnn_arch.py
@@ -8,30 +8,45 @@ from models.archs.classSR_fsrcnn_arch import classSR_3class_fsrcnn_net
 
 from typing import Type, List, Union, Tuple
 
+from brt.runtime.proto_tensor import (
+    make_proto_tensor_from,
+    to_torch_tensor,
+    collect_proto_attr_stack,
+    init_proto_tensor,
+)
+
 from brt.router import ScatterRouter, GatherRouter
 
 
 class classSR_3class_fused_fsrcnn_net(nn.Module):
-    def __init__(self, raw: classSR_3class_fsrcnn_net, subnet_bs: Tuple[int]=(34, 38, 29) ):
+    def __init__(
+        self, raw: classSR_3class_fsrcnn_net, subnet_bs: Tuple[int] = (34, 38, 29)
+    ):
         super(classSR_3class_fused_fsrcnn_net, self).__init__()
-        self.upscale = 4
-        self.classifier = Classifier()
-        self.net1 = Fused_FSRCNN_net(raw.net1, subnet_bs[0])
-        self.net2 = Fused_FSRCNN_net(raw.net2, subnet_bs[1])
-        self.net3 = Fused_FSRCNN_net(raw.net3, subnet_bs[2])
+        self.subnet_bs = subnet_bs
+        self.upscale = raw.upscale
+        self.classifier = raw.classifier
         self.scatter_router = ScatterRouter(
             protocol_type="topk", protocol_kwargs={"top_k": 1}
         )
+        self.net1 = Fused_FSRCNN_net(raw.net1, subnet_bs[0])
+        self.net2 = Fused_FSRCNN_net(raw.net2, subnet_bs[1])
+        self.net3 = Fused_FSRCNN_net(raw.net3, subnet_bs[2])
         self.gather_router = GatherRouter(fabric_type="combine")
 
     def forward(self, x, is_train=False):
 
         weights = self.classifier(x)
-        sr_x = self.scatter_router(x, weights)
-        y = [self.net1(sr_x[0]), self.net2(sr_x[1]), self.net3(sr_x[2])]
-        gr_x = self.gather_router(y)
-        return gr_x, [yy.shape[0] for yy in y]
-        
+        sr_xs = self.scatter_router(x, weights)
+        real_bs = [srx.shape[0] for srx in sr_xs] 
+        proto_info = [collect_proto_attr_stack(srx) for srx in sr_xs]
+        sr_xs = [srx.resize_([bs, *srx.shape[1:]]) for bs, srx in zip(self.subnet_bs, sr_xs)]
+        y = [self.net1(sr_xs[0]), self.net2(sr_xs[1]), self.net3(sr_xs[2])]
+        for i in range(3):
+            y[i] = init_proto_tensor(y[i][:real_bs[i]], *proto_info[i])
+        gr_xs = self.gather_router(y)
+        return gr_xs, [yy.shape[0] for yy in y]
+
 
 class Classifier(nn.Module):
     def __init__(self):
@@ -55,7 +70,7 @@ class Classifier(nn.Module):
 
     def forward(self, x):
         # assert x.shape[1:] == torch.Size([3, 32, 32]), x.shape
-        out = self.CondNet(x) # [bs, 32, 8, 8]
+        out = self.CondNet(x)  # [bs, 32, 8, 8]
         out = self.avgPool2d(out)  # [bs, 32, 1, 1]
         out = out.view(-1, 32)  # [bs, 32]
         out = self.lastOut(out)  # [bs, 3]
