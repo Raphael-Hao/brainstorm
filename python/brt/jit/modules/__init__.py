6ff2f85a6674bfa51c13349d17e7896394470a11
Lingji Ouyang <ouyanglingji@gmail.com>
2022-10-08 08:42:32 +0000

    change ModuleInfo to support Conv2d + element-wise op

--- python/brt/jit/modules/__init__.py
+++ python/brt/jit/modules/__init__.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2022 by Microsoft Corporation.
 # Licensed under the MIT license.
 from brt.jit.modules.linear import LinearInfo
-from brt.jit.modules.conv2d_batchnorm2d_relu import Conv2dBatchNorm2dReLUInfo
+from brt.jit.modules.conv2d_element_wise import Conv2dElementWiseInfo
 from brt.jit.modules.conv2d_prelu import Conv2dPReLUInfo
 from brt.jit.modules.convtranspose2d import ConvTranspose2dInfo
 from brt.jit.modules.base import ModuleInfo
