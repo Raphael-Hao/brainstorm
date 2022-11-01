3cdb3846637f05e4f90b41ae8b2d89351946de9c
raphael <raphaelhao@outlook.com>
2022-11-01 14:49:52 +0000

    update

--- docker/install.sh
+++ docker/install.sh
@@ -16,8 +16,7 @@ apt-get update && apt-get install -y python3 python3-dev python3-setuptools \
     gcc libtinfo-dev zlib1g-dev build-essential \
     cmake libedit-dev libxml2-dev llvm tmux
 
-mkdir -p /root/brainstorm_project
-cd /root/brainstorm_project
+cd /root
 git clone git@github.com:Raphael-Hao/brainstorm.git \
     -b "${BRT_BRANCH:-main}" \
     --recursive --depth 1
