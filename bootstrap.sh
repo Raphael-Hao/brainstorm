9f066e605929cc383fc5591083c4c6cdb852722e
raphael <raphaelhao@outlook.com>
2022-11-07 08:05:03 -0800

    update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,28 +1,41 @@
 #!/usr/bin/env bash
-# Motto: Were It to Benefit My Country, I Would Lay Down My Life!
-# \file: /install.sh
-# \brief:
-# Author: raphael hao
+# Copyright (c) 2022 by Microsoft Corporation.
+# Licensed under the MIT license.
+
 set -e
 set -u
 set -o pipefail
+
+cd "$HOME"
+wget https://azcopyvnext.azureedge.net/release20221005/azcopy_linux_amd64_10.16.1.tar.gz -O azcopy.tar.gz
+mkdir azcopy && tar -xzvf azcopy.tar.gz -C "$HOME/azcopy" --strip-components=1
+mv azcopy/azcopy /usr/bin/azcopy && rm -rf azcopy.tar.gz azcopy
+
+wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+sudo apt-get update
+sudo apt-get install blobfuse
+
+git clone git@github.com:Raphael-Hao/dotfile.git ~/dotfile
+cd ~/dotfile
+
+
 
 if [[ "$1" == "--branch" ]]; then
     BRT_BRANCH="$2"
     shift 2
 fi
 
-apt-get -y update && apt-get install -y \
-    ssh gcc libtinfo-dev zlib1g-dev build-essential \
-    cmake libedit-dev libxml2-dev llvm tmux wget git
 
 cd /root
 wget https://repo.anaconda.com/miniconda/Miniconda3-py38_4.10.3-Linux-x86_64.sh &&
     bash Miniconda3-py38_4.10.3-Linux-x86_64.sh -b -p /opt/miniconda3 &&
     rm -f Miniconda3-py38_4.10.3-Linux-x86_64.sh
-echo 'export PATH=/opt/miniconda3/bin:$PATH' >> /etc/profile
+echo 'export PATH=/opt/miniconda3/bin:$PATH' >> ~/.bashrc
+echo 'export PATH=/opt/miniconda3/bin:$PATH' >> ~/.zshrc
+source/.bashrc
 
-mkdir -p /brainstorm_project && cd /brainstorm_project
+mkdir -p ~/brainstorm_project && cd ~/brainstorm_project
 git clone git@github.com:Raphael-Hao/brainstorm.git \
     -b "${BRT_BRANCH:-main}" \
     --recursive --depth 1
