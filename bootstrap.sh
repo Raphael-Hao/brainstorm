613de6c184cd31810494f1e476565ec5a9e8121a
raphael <raphaelhao@outlook.com>
2022-11-07 17:18:59 -0800

    update

--- bootstrap.sh
+++ bootstrap.sh
@@ -25,41 +25,41 @@ mkdir ~/largedata
 blobfuse ~/datasets/swin_moe --tmp-path=/mnt/ramdisk/blobfusetmp  --config-file=azure/blob/blobfuse.cfg -o attr_timeout=240 -o entry_timeout=240 -o negative_timeout=120
 
 
-if [[ "$1" == "--branch" ]]; then
-    BRT_BRANCH="$2"
-    shift 2
-fi
+# if [[ "$1" == "--branch" ]]; then
+#     BRT_BRANCH="$2"
+#     shift 2
+# fi
 
 
-cd /root
-wget https://repo.anaconda.com/miniconda/Miniconda3-py38_4.10.3-Linux-x86_64.sh &&
-    bash Miniconda3-py38_4.10.3-Linux-x86_64.sh -b -p /opt/miniconda3 &&
-    rm -f Miniconda3-py38_4.10.3-Linux-x86_64.sh
-echo 'export PATH=/opt/miniconda3/bin:$PATH' >> ~/.bashrc
-echo 'export PATH=/opt/miniconda3/bin:$PATH' >> ~/.zshrc
-source/.bashrc
+# cd /root
+# wget https://repo.anaconda.com/miniconda/Miniconda3-py38_4.10.3-Linux-x86_64.sh &&
+#     bash Miniconda3-py38_4.10.3-Linux-x86_64.sh -b -p /opt/miniconda3 &&
+#     rm -f Miniconda3-py38_4.10.3-Linux-x86_64.sh
+# echo 'export PATH=/opt/miniconda3/bin:$PATH' >> ~/.bashrc
+# echo 'export PATH=/opt/miniconda3/bin:$PATH' >> ~/.zshrc
+# source/.bashrc
 
-mkdir -p ~/brainstorm_project && cd ~/brainstorm_project
-git clone git@github.com:Raphael-Hao/brainstorm.git \
-    -b "${BRT_BRANCH:-main}" \
-    --recursive --depth 1
-cd brainstorm
+# mkdir -p ~/brainstorm_project && cd ~/brainstorm_project
+# git clone git@github.com:Raphael-Hao/brainstorm.git \
+#     -b "${BRT_BRANCH:-main}" \
+#     --recursive --depth 1
+# cd brainstorm
 
-pip install --upgrade pip
-pip install torch==1.12.1+cu113 torchvision==0.13.1+cu113 torchaudio==0.12.1 --extra-index-url https://download.pytorch.org/whl/cu113
-pip install -r requirements.txt
+# pip install --upgrade pip
+# pip install torch==1.12.1+cu113 torchvision==0.13.1+cu113 torchaudio==0.12.1 --extra-index-url https://download.pytorch.org/whl/cu113
+# pip install -r requirements.txt
 
-cd 3rdparty/tvm || exit
+# cd 3rdparty/tvm || exit
 
-mkdir -p build && cd build || exit
-cp ../../../cmake/config/tvm.cmake config.cmake
-cmake ..
-export LD_LIBRARY_PATH=/usr/local/cuda/lib64/stubs:$LD_LIBRARY_PATH && \
-    ln -s /usr/local/cuda/lib64/stubs/libcuda.so /usr/local/cuda/lib64/stubs/libcuda.so.1 && \
-    make install -j && \
-    rm -f /usr/local/cuda/lib64/stubs/libcuda.so.1
-cd ../python && pip install .
+# mkdir -p build && cd build || exit
+# cp ../../../cmake/config/tvm.cmake config.cmake
+# cmake ..
+# export LD_LIBRARY_PATH=/usr/local/cuda/lib64/stubs:$LD_LIBRARY_PATH && \
+#     ln -s /usr/local/cuda/lib64/stubs/libcuda.so /usr/local/cuda/lib64/stubs/libcuda.so.1 && \
+#     make install -j && \
+#     rm -f /usr/local/cuda/lib64/stubs/libcuda.so.1
+# cd ../python && pip install .
 
-cd /brainstorm_project/brainstorm || exit
+# cd /brainstorm_project/brainstorm || exit
 
-pip install -v --editable .
+# pip install -v --editable .
