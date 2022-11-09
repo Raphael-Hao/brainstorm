6cc214199a193b4eb4a25554240637620b031d26
raphael <raphaelhao@outlook.com>
2022-11-09 07:03:54 +0000

    data exchange python interface

--- python/brt/runtime/distributed.py
+++ python/brt/runtime/distributed.py
@@ -1,6 +1,6 @@
 # Copyright (c) 2022 by Microsoft Corporation.
 # Licensed under the MIT license.
-from typing import Tuple, overload, Literal
+from typing import Tuple, overload, Literal, List
 
 import brt._C.distributed as C_dist
 import torch
@@ -13,6 +13,22 @@ def init_nccl(group: dist.ProcessGroup):
     unique_id = C_dist.make_nccl_unique_id(world_rank)
     dist.broadcast(unique_id, 0, group)
     C_dist.init_nccl(unique_id, world_rank, world_size)
+
+
+def exchange(in_data: torch.Tensor, reorder_indices: torch.Tensor):
+    return C_dist.exchange(in_data, reorder_indices)
+
+
+def batch_exchange(in_datas: List[torch.Tensor], reorder_indices: torch.Tensor):
+    return C_dist.batch_exchange(in_datas, reorder_indices)
+
+
+def reverse_exchange(in_data: torch.Tensor, reorder_indices: torch.Tensor):
+    return C_dist.reverse_exchange(in_data, reorder_indices)
+
+
+def batch_reverse_exchange(in_datas: List[torch.Tensor], reorder_indices: torch.Tensor):
+    return C_dist.batch_reverse_exchange(in_datas, reorder_indices)
 
 
 @overload
@@ -34,6 +50,7 @@ def asymmetry_a2a(
 ):
     return C_dist.asymmetry_all_to_all(in_data, in_loads, locality_aware)
 
+
 @overload
 def group_asymmetry_a2a(
     in_data: torch.Tensor, in_loads: torch.Tensor, locality_aware: Literal[False]
@@ -52,3 +69,29 @@ def group_asymmetry_a2a(
     in_data: torch.Tensor, in_loads: torch.Tensor, locality_aware: bool = False
 ):
     return C_dist.group_asymmetry_all_to_all(in_data, in_loads, locality_aware)
+
+
+@overload
+def batch_group_asymmetry_a2a(
+    in_datas: List[torch.Tensor], in_loads: torch.Tensor, locality_aware: Literal[False]
+) -> Tuple[List[torch.Tensor], torch.Tensor]:
+    ...
+
+
+@overload
+def batch_group_asymmetry_a2a(
+    in_datas: List[torch.Tensor], in_loads: torch.Tensor, locality_aware: Literal[True]
+) -> Tuple[List[torch.Tensor], torch.Tensor, torch.Tensor]:
+    ...
+
+
+def batch_group_asymmetry_a2a(
+    in_datas: List[torch.Tensor], in_loads: torch.Tensor, locality_aware: bool = False
+):
+    out_datas, out_loads, reorder_indices = C_dist.batch_group_asymmetry_all_to_all(
+        in_datas, in_loads, locality_aware
+    )
+    if locality_aware:
+        return out_datas, out_loads, reorder_indices
+    else:
+        return out_datas, out_loads
