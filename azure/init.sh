dd2dbc31538d9706728b362b429c4a591bdc720f
raphael <raphaelhao@outlook.com>
2022-11-09 06:43:09 -0800

    update

--- azure/init.sh
+++ azure/init.sh
@@ -4,8 +4,52 @@
 # \brief:
 # Author: raphael hao
 
-set -e
+is_root() {
+    return "$(id -u)"
+}
 
-cp -r ./vscode ../.vscode
+has_sudo() {
+    local prompt
+    prompt=$(sudo -nv 2>&1)
+    if [ $? -eq 0 ]; then
+        echo "has_sudo__pass_set"
+    elif echo "$prompt" | grep -q '^sudo:'; then
+        echo "has_sudo__needs_pass"
+    else
+        echo "no_sudo"
+    fi
+}
+
+if is_root; then
+    use_sudo=1
+    sudo_cmd=""
+else
+    HAS_SUDO=$(has_sudo)
+    sudo_cmd="sudo"
+    if [ "$HAS_SUDO" == "has_sudo__needs_pass" ]; then
+        use_sudo=1
+        echo "You need to supply the password to use sudo."
+        sudo -v
+    elif [ "$HAS_SUDO" == "has_sudo__pass_set" ]; then
+        use_sudo=1
+    else
+        echo "You need to have sudo privileges to run this script for some packages."
+        use_sudo=0
+        exit 1
+    fi
+fi
+
+
+wget https://azcopyvnext.azureedge.net/release20221005/azcopy_linux_amd64_10.16.1.tar.gz -O azcopy.tar.gz
+mkdir -p azcopy && tar -xzvf azcopy.tar.gz -C "azcopy" --strip-components=1
+$sudo_cmd mv azcopy/azcopy /usr/bin/azcopy && rm -rf azcopy.tar.gz azcopy
+
+UBUNTU_DIST=$(lsb_release -sr)
+wget https://packages.microsoft.com/config/ubuntu/"${UBUNTU_DIST}"/packages-microsoft-prod.deb
+$sudo_cmd dpkg -i packages-microsoft-prod.deb && rm -f packages-microsoft-prod.deb
+$sudo_cmd apt-get update && $sudo_cmd apt-get install blobfuse dotnet-sdk-6.0
+
+mkdir -p ../.vscode
+cp -r ./vscode/* ../.vscode/
 bash blob/mount.sh
 bash blob/prepare_dataset_ckpt.sh
