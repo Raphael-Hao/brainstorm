83f3f40c5d9d68149410227ebe70d8fd6e5102e5
OyLingji <102283259+OyLingji@users.noreply.github.com>
2022-11-22 17:36:56 +0800

    Refactor jit factory (#18)
    * make_jit_kernel now supports rank list
    
    * [brt] refactor brt.jit.module
    
    * [brt.jit] bug fix
    
    * [brt.jit] refactor & complete make_jit_kernel for opt_level = None & horiz_fuse
    
    * [jit] add make_kernel method
    
    * [jit] bug fix
    
    * [jit] add make_function for atom module
    
    * [jit] add make_function for hetero fusion
    
    * [jit] add test for make_function
    
    * [jit] add make_fucntion for horiz_fuse

--- python/brt/jit/modules/__init__.py
+++ python/brt/jit/modules/__init__.py
@@ -1,7 +1,28 @@
 # Copyright (c) 2022 by Microsoft Corporation.
 # Licensed under the MIT license.
-from brt.jit.modules.linear import LinearInfo
-from brt.jit.modules.conv2d_element_wise import Conv2dElementWiseInfo
-from brt.jit.modules.conv2d_prelu import Conv2dPReLUInfo
-from brt.jit.modules.convtranspose2d import ConvTranspose2dInfo
-from brt.jit.modules.base import ModuleInfo
+
+__all__ = [
+    "JitModuleFactory",
+    "ModuleBase",
+    "AtomModule",
+    "LinearModule",
+    "Conv2dElementWiseModule",
+    "FusedModule",
+    "HorizFusedModule",
+    "HeteroFusedModule",
+    "HomoFusedModule",
+]
+
+
+from brt.jit.modules.factory import JitModuleFactory
+
+from brt.jit.modules.base import ModuleBase
+
+from brt.jit.modules.atom import AtomModule
+from brt.jit.modules.linear import LinearModule
+from brt.jit.modules.conv2d_element_wise import Conv2dElementWiseModule
+
+from brt.jit.modules.fused import FusedModule
+from brt.jit.modules.horiz import HorizFusedModule
+from brt.jit.modules.hetero import HeteroFusedModule
+from brt.jit.modules.homo import HomoFusedModule
