f389ea6d58b4db2613a7afcd7bd5fbe58212047f
Raphael-Hao <raphaelhao@outlook.com>
2023-04-18 11:55:07 +0800

    update docker and fix circular dependency

---
+++ docker/docker_build.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# Motto: Were It to Benefit My Country, I Would Lay Down My Life!
+# \file: /build.py
+# \brief:
+# Author: raphael hao
+import argparse
+import pathlib
+import subprocess
+
+import yaml
+
+
+def get_build_args():
+    parser = argparse.ArgumentParser(description="Build docker image")
+    parser.add_argument(
+        "--config",
+        type=str,
+        default="config.yaml",
+        help="Config file for docker build",
+    )
+    args = parser.parse_args()
+
+    script_dir = pathlib.Path(__file__).parent.absolute()
+    config_file = script_dir / str(args.config)
+    config = yaml.safe_load(config_file.open())
+
+    args.type = config["type"]
+    args.username = config["username"]
+    args.token = config["token"]
+    args.base_image = config["base_image"]
+    args.tag = config["tag"]
+    args.branch = config["branch"]
+    args.upload = config["upload"]
+    args.updata_brt = config["update_brt"]
+    args.no_cache = config["no_cache"]
+    args.context = config["context"]
+    context_dir = script_dir if args.context is None else pathlib.Path(args.context)
+    args.context_path = context_dir.as_posix()
+    args.dockerfile = (context_dir / f"Dockerfile.{args.type}").as_posix()
+    args.image_spec = f"brt:{args.branch}" if args.tag is None else f"brt:{args.tag}"
+
+    return args
+
+
+def login_github_registry(username, token):
+    az_acr_login_cmd = [
+        "echo",
+        token,
+        "|",
+        "docker",
+        "login",
+        "ghcr.io",
+        "-u",
+        username,
+        "--password-stdin",
+    ]
+    subprocess.call(az_acr_login_cmd)
+
+
+def docker_upload(image_spec, username):
+    docker_tag_cmd = ["docker", "tag", image_spec]
+    new_tag = f"ghcr.io/{username}/{image_spec}"
+    print(f"Tagging image {image_spec} with tag: {new_tag}")
+    docker_tag_cmd.append(new_tag)
+    subprocess.call(docker_tag_cmd)
+    docker_push_cmd = ["docker", "push", new_tag]
+    print(f"Pushing image: {new_tag} to github registry")
+    subprocess.call(docker_push_cmd)
+
+
+def build_docker():
+    args = get_build_args()
+    login_github_registry(args.username, args.token)
+    cmd = [
+        "docker",
+        "build",
+        "-t",
+        args.image_spec,
+    ]
+
+    cmd.extend(["--build-arg", f"BASE_IMAGE={args.base_image}"])
+    cmd.extend(["--build-arg", f"BRT_BRANCH={args.branch}"])
+    if args.type == "update":
+        cmd.extend(["--build-arg", f"UPDATE_BRT_ONLY={args.update_brt}"])
+        cmd.append("--no-cache")
+    if args.no_cache and "--no-cache" not in cmd:
+        cmd.extend(["--no-cache"])
+    cmd.extend(["-f", args.dockerfile])
+    cmd.extend(["--progress=plain", args.context_path])
+    print(" ".join(cmd))
+    print("Building docker image...")
+    print(f"Image spec: {args.image_spec}")
+    print(f"Base image: {args.base_image}")
+    print(f"Using Branch: {args.branch} of BRT")
+    print(f"Using Dockerfile: {args.dockerfile}")
+    print(f"Using context: {args.context_path}")
+    subprocess.call(cmd)
+
+    if args.upload:
+        docker_upload(args.image_spec, args.username)
+
+
+if __name__ == "__main__":
+    build_docker()
--- docker/install.sh
+++ docker/install.sh
@@ -13,28 +13,41 @@ if [[ "$1" == "--branch" ]]; then
 fi
 
 cd /root
+
+wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
+sed '/exec zsh -l/d' ./install.sh >./install_wo_exec.sh
+sh install_wo_exec.sh
+rm install.sh install_wo_exec.sh
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
+sed "s/plugins=(git)/plugins=(git extract zsh-autosuggestions zsh-history-substring-search zsh-syntax-highlighting)/g" "${HOME}/.zshrc" >"${HOME}/.tmp_zshrc" && mv "${HOME}/.tmp_zshrc" "${HOME}/.zshrc"
+
+
+
 wget https://repo.anaconda.com/miniconda/Miniconda3-py38_4.10.3-Linux-x86_64.sh &&
     bash Miniconda3-py38_4.10.3-Linux-x86_64.sh -b -p /opt/miniconda3 &&
     rm -f Miniconda3-py38_4.10.3-Linux-x86_64.sh
 
 cd /root
-git clone git@github.com:Raphael-Hao/dotfile.git
-cd dotfile
-echo -e "USE_BREW=OFF\nUSE_SYS_APPS=ON\nUSE_SHELL=ALL\nUSE_TMUX=ON\nUSE_VIM=OFF\nUSE_PYENV=ON\nUSE_CMAKE=3.23.2\nUSE_LLVM=OFF\nUSE_GTEST=OFF\nUSE_VSCODE=OFF" > dot.conf
-bash bootstrap.sh
 
 echo 'export PATH=/opt/miniconda3/bin:$PATH' >>/etc/profile
 
+pip config set global.index-url https://mirror.sjtu.edu.cn/pypi/web/simple
+pip install --upgrade pip
+pip install torch==1.12.1+cu113 torchvision==0.13.1+cu113 torchaudio==0.12.1 -f https://mirror.sjtu.edu.cn/pytorch-wheels/torch_stable.html
+
+
 BRT_DIR=/brainstorm_project/brainstorm
 mkdir -p /brainstorm_project && cd /brainstorm_project
-git clone git@github.com:Raphael-Hao/brainstorm.git \
+
+git clone https://Raphael-Hao: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@github.com/Raphael-Hao/brainstorm.git \
     -b "${BRT_BRANCH:-main}" \
     --recursive
 
 cd "$BRT_DIR" || exit
 
-pip install --upgrade pip
-pip install torch==1.12.1+cu113 torchvision==0.13.1+cu113 torchaudio==0.12.1 --extra-index-url https://download.pytorch.org/whl/cu113
 pip install -r requirements.txt
 pip install -v --editable .
 
