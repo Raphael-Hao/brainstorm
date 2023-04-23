2877173c726335838f8d1efc9e2077cc85ccaef1
Raphael-Hao <raphaelhao@outlook.com>
2023-04-23 14:54:57 +0800

    update

--- scripts/setup_bare.sh
+++ scripts/setup_bare.sh
@@ -9,9 +9,43 @@ if [[ "$1" == "--branch" ]]; then
     shift 2
 fi
 
+
+is_root() {
+    return "$(id -u)"
+}
+
+has_sudo() {
+    local prompt
+    prompt=$(sudo -nv 2>&1)
+    if [ $? -eq 0 ]; then
+        echo "has_sudo__pass_set"
+    elif echo "$prompt" | grep -q '^sudo:'; then
+        echo "has_sudo__needs_pass"
+    else
+        echo "no_sudo"
+    fi
+}
+
+if is_root; then
+    sudo_cmd=""
+else
+    HAS_SUDO=$(has_sudo)
+    sudo_cmd="sudo"
+    if [ "$HAS_SUDO" == "has_sudo__needs_pass" ]; then
+        echo "You need to supply the password to use sudo."
+        sudo -v
+    elif [ "$HAS_SUDO" == "has_sudo__pass_set" ]; then
+        echo "You already have sudo privileges."
+    else
+        echo "You need to have sudo privileges to run this script for some packages."
+        exit 1
+    fi
+fi
+
+
 cd "$HOME" || exit
 
-apt-get -y update && apt-get install -y \
+$sudo_cmd apt-get -y update && $sudo_cmd apt-get install -y \
     ssh openssh-server gcc libtinfo-dev zlib1g-dev build-essential \
     cmake libedit-dev libxml2-dev llvm tmux wget curl git vim zsh
 
@@ -26,13 +60,13 @@ git clone https://github.com/zsh-users/zsh-history-substring-search "${ZSH_CUSTO
 sed "s/plugins=(git)/plugins=(git extract zsh-autosuggestions zsh-history-substring-search zsh-syntax-highlighting)/g" "${HOME}/.zshrc" >"${HOME}/.tmp_zshrc" && mv "${HOME}/.tmp_zshrc" "${HOME}/.zshrc"
 
 wget https://repo.anaconda.com/miniconda/Miniconda3-py38_4.10.3-Linux-x86_64.sh &&
-    bash Miniconda3-py38_4.10.3-Linux-x86_64.sh -b -p /opt/miniconda3 &&
+    bash Miniconda3-py38_4.10.3-Linux-x86_64.sh -b -p "$HOME"/miniconda3 &&
     rm -f Miniconda3-py38_4.10.3-Linux-x86_64.sh
 
 # shellcheck disable=SC2016
-echo 'export PATH=/opt/miniconda3/bin:$PATH' >>"$HOME/.zshrc"
+echo 'export PATH="$HOME"/miniconda3/bin:$PATH' >>"$HOME/.zshrc"
 # shellcheck disable=SC2016
-echo 'export PATH=/opt/miniconda3/bin:$PATH' >>"$HOME/.bashrc"
+echo 'export PATH="$HOME"/miniconda3/bin:$PATH' >>"$HOME/.bashrc"
 
 # shellcheck disable=SC1090,SC1091
 source "$HOME/.bashrc"
@@ -56,10 +90,7 @@ cd 3rdparty/tvm || exit
 mkdir -p build && cd build || exit
 cp ../../../cmake/config/tvm.cmake config.cmake
 cmake ..
-export LD_LIBRARY_PATH=/usr/local/cuda/lib64/stubs:$LD_LIBRARY_PATH &&
-    ln -s /usr/local/cuda/lib64/stubs/libcuda.so /usr/local/cuda/lib64/stubs/libcuda.so.1 &&
-    make install -j &&
-    rm -f /usr/local/cuda/lib64/stubs/libcuda.so.1
+make install -j24
 cd ../python && pip install .
 cd "$BRT_DIR" || exit
 
